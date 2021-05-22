2c2966871ef910d99212582db6edec45483d7766
spotlessmind1975 <asimov@mclink.it>
2021-05-22 16:42:51 +0000

    Added specific vars on c64.

--- ugbc/src/hw/c64/vars.asm
+++ ugbc/src/hw/c64/vars.asm
@@ -1,1 +1,8 @@
 TEXTADDRESS:        .BYTE   0, 4
+COLORADDRESS:       .BYTE   0, $D8
+TABCOUNT:   .BYTE    4
+LOCALPEN:   .BYTE    0
+LOCALPAPER: .BYTE    7
+XCURS:      .BYTE    0
+YCURS:      .BYTE    0
+LOCALWW:    .BYTE    3
