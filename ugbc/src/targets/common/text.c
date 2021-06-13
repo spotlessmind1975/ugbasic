cf3e0846b0a5339e8b0a5dcfc2a68b35028df8ad
spotlessmind1975 <asimov@mclink.it>
2021-06-13 16:32:27 +0000

    All environment variables for text output are global now (on c64).

--- ugbc/src/targets/common/text.c
+++ ugbc/src/targets/common/text.c
@@ -95,22 +95,39 @@ windowX_cy:	.res 1
 void setup_text_variables( Environment * _environment ) {
 
     variable_define( _environment, "windowX", VT_BYTE, 0 );
+    variable_global( _environment, "windowX" );
     variable_define( _environment, "windowY", VT_BYTE, 0 );                
+    variable_global( _environment, "windowY" );
     variable_define( _environment, "windowX2", VT_BYTE, (TEXT_COLUMNS_COUNT-1) );
+    variable_global( _environment, "windowX2" );
     variable_define( _environment, "windowY2", VT_BYTE, (TEXT_ROWS_COUNT-1) );                
+    variable_global( _environment, "windowY2" );
     variable_define( _environment, "windowCX", VT_BYTE, 0 );
+    variable_global( _environment, "windowCX" );
     variable_define( _environment, "windowCY", VT_BYTE, 0 );
-    variable_define( _environment, "PEN", VT_COLOR, COLOR_BLACK );
-    variable_define( _environment, "PAPER", VT_COLOR, COLOR_WHITE );
+    variable_global( _environment, "windowCY" );
+    variable_define( _environment, "PEN", VT_COLOR, COLOR_WHITE );
+    variable_global( _environment, "PEN" );
+    variable_define( _environment, "PAPER", VT_COLOR, COLOR_BLACK );
+    variable_global( _environment, "PAPER" );
     variable_define( _environment, "windowE", VT_BYTE, 0 );
+    variable_global( _environment, "windowE" );
     variable_define( _environment, "windowS", VT_BYTE, 0 );
+    variable_global( _environment, "windowS" );
     variable_define( _environment, "windowW", VT_BYTE, 0 );
+    variable_global( _environment, "windowW" );
     variable_define( _environment, "windowT", VT_BYTE, 4 );
+    variable_global( _environment, "windowT" );
     variable_define( _environment, "windowWW", VT_BYTE, WW_PEN | WW_PAPER );
+    variable_global( _environment, "windowWW" );
     variable_define( _environment, "TAB", VT_STRING, 0 );
     variable_store_string( _environment, "TAB", "\t");
+    variable_global( _environment, "TAB" );
+    variable_global( _environment, "windowT" );
     variable_define( _environment, "windowMX", VT_BYTE, 0 );
+    variable_global( _environment, "windowMX" );
     variable_define( _environment, "windowMY", VT_BYTE, 0 );                
+    variable_global( _environment, "windowMY" );
 
 }
 
@@ -509,6 +526,8 @@ void text_set_tab( Environment * _environment, char * _new_tab ) {
 }
 
 void text_at( Environment * _environment, char * _x, char * _y, char * _text ) {
+
+    setup_text_variables( _environment );
 
     text_locate( _environment, _x, _y );
 
