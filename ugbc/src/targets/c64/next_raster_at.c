2c8b84e5b3951037f74865b3fb4899b133012b73
spotlessmind1975 <asimov@mclink.it>
2021-07-06 14:30:50 +0000

    Refactorized RASTER keywords.

--- ugbc/src/targets/c64/next_raster_at.c
+++ ugbc/src/targets/c64/next_raster_at.c
@@ -39,62 +39,7 @@
  ****************************************************************************/
 
 /**
- * @brief Emit ASM code for <b>RASTER AT [int]x WITH label</b>
- * 
- * This function outputs assembly code needed to initialize a raster routine. 
- * In other words, asynchronously and in parallel with the execution of the 
- * main program, the routine starting from the label provided will be executed
- * when the vertical brush on the screen reaches the value of _position. 
- * This function is particularly useful when the position is communicated 
- * is given as an integer.
- * 
- * @param _environment Current calling environment
- * @param _label Label to jump to when vertical raster reach the value given
- * @param _position The vertical position to wait for
- */
-void raster_at( Environment * _environment, char * _label, int _position ) {
-    
-    outline2("; RASTER AT %d WITH %s (ignored)", _position, _label);
-
-}
-
-/**
- * @brief Emit ASM code for <b>RASTER AT [expression] WITH label</b>
- * 
- * This function outputs assembly code needed to initialize a raster routine. 
- * In other words, asynchronously and in parallel with the execution of the 
- * main program, the routine starting from the label provided will be executed
- * when the vertical brush on the screen reaches the value of _position. 
- * This function is particularly useful when the position is communicated 
- * is given as an expression
- * 
- * @param _environment Current calling environment
- * @param _label Label to jump to when vertical raster reach the value given
- * @param _position The vertical position to wait for
- */
-void raster_at_var( Environment * _environment, char * _label, char * _position ) {
-   
-    outline2("; RASTER AT %s WITH %s (ignored)", _position, _label);
-
-}
-
-/**
- * @brief Emit ASM code for <b>NEXT RASTER</b>
- * 
- * This function outputs assembly code needed to wait for the
- * next raster. Meanwhile, the execution of the main code will resume 
- * where it left off.
- * 
- * @param _environment Current calling environment
- */
-void next_raster( Environment * _environment ) {
-
-    outline0("; NEXT RASTER (ignored)");
-
-}
-
-/**
- * @brief Emit ASM code for <b>NEXT RASTER AT [int]x WITH label</b>
+ * @brief Emit ASM code for <b>NEXT RASTER AT [int] WITH [label]</b>
  * 
  * This function outputs a code that puts the raster routine on hold for the 
  * vertical raster to arrive at a new _position with the execution of a different 
@@ -105,9 +50,34 @@ void next_raster( Environment * _environment ) {
  * @param _label Label to jump to when vertical raster reach the value given
  * @param _position The vertical position to wait for
  */
+/* <usermanual>
+@keyword NEXT RASTER AT
+
+@english
+Wait for the next raster at a specific, new, raster position
+and to execute the code at the given label. Meanwhile, the 
+execution of the main code will resume where it left off.
+
+@italian
+Attendi il raster successivo in una nuova posizione raster specifica
+ed esegui il codice in corrispondenza dell'etichetta data. Nel 
+frattempo, l'esecuzione del codice principale riprenderà da dove 
+era stata interrotta.
+
+@syntax NEXT RASTER AT # [integer] WITH [label]
+
+@example NEXT RASTER AT #$42 WITH myRasterRoutine
+
+@target c64
+</usermanual> */
 void next_raster_at_with( Environment * _environment, int _position, char * _label ) {
     
-    outline2("; RASTER AT %d WITH %s (ignored)", _position, _label);
+    outline2("; RASTER AT %d WITH %s", _position, _label);
+
+    char positionlo[MAX_TEMPORARY_STORAGE]; sprintf( positionlo, "%2.2x", (unsigned char) ( _position & 0xff )  );
+    char positionhi[MAX_TEMPORARY_STORAGE]; sprintf( positionhi, "%2.2x", (unsigned char) ( ( ( _position >> 8 ) & 0x01 ) << 8 ) );
+
+    vic2_next_raster_at( _environment, _label, positionlo, positionhi );
 
 }
 
@@ -123,8 +93,24 @@ void next_raster_at_with( Environment * _environment, int _position, char * _lab
  * @param _label Label to jump to when vertical raster reach the value given
  * @param _position The vertical position to wait for
  */
+/* <usermanual>
+@keyword NEXT RASTER AT
+
+@syntax NEXT RASTER AT [expression] WITH [label]
+
+@example NEXT RASTER AT newRaterLine WITH myRasterRoutine
+</usermanual> */
 void next_raster_at_with_var( Environment * _environment, char * _position, char * _label ) {
 
-    outline2("; NEXT RASTER AT %s WITH %s (ignored)", _position, _label);
+    outline2("; NEXT RASTER AT %s WITH %s", _position, _label);
+
+    MAKE_LABEL
+
+    Variable * var = variable_retrieve( _environment, _position );
+
+    char positionlo[MAX_TEMPORARY_STORAGE]; sprintf( positionlo, "%s", var->realName );
+    char positionhi[MAX_TEMPORARY_STORAGE]; sprintf( positionhi, "%s+1", var->realName );
+
+    vic2_next_raster_at( _environment, _label, positionlo, positionhi );
 
 }
