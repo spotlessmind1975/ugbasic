0bbadd05689b30cd08882c9017f2d45a949c6724
spotlessmind1975 <asimov@mclink.it>
2021-07-15 09:05:24 +0000

    Updated docs.

--- ugbc/src/targets/common/draw.c
+++ ugbc/src/targets/common/draw.c
@@ -38,6 +38,54 @@
  * CODE SECTION 
  ****************************************************************************/
 
+/**
+ * @brief Emit ASM code to implement <strong>DRAW</strong> command
+ * 
+ * @param _environment Current calling environment
+ * @param _x1 Left abscissa of the line
+ * @param _y1 Left ordinate of the line
+ * @param _x2 Right abscissa of the line
+ * @param _y2 Right ordinate of the line
+ * @param _c Color to use for drawing the line
+ */
+/* <usermanual>
+@keyword DRAW
+
+@english
+This instruction draws aline on the screen, starting from the coordinates ''(x1, y1)''
+to arrive at the coordinates ''(x2, y2)'', using the color ''c''. The start or the final
+coordinates can be omitted: in this case, ugBASIC will draw, respectively, starting from 
+the last drawn position and arriving at the last drawn position. If the color is 
+omitted, the last color selected with the ''INK'' or ''PEN'' command will be used. 
+Finally, the default line style is full but a 16 bit bitmask can be set with the ''SET LINE''
+command.
+
+@italian
+Questa istruzione disegna un segmento di linea sullo schermo, a partire dalle coordinate ''(x1,y1)''
+per arrivare alle coordinate ''(x2,y2)'', utilizzando il colore ''c''. Le coordinate di inizio o
+quelle di fine possono essere omesse: in tal caso, ugBASIC disegnerà, rispettivamente, 
+ùa partire dall'ultima posizione disegnata arrivando all'ultima posizione disegnata. 
+Se il colore viene omesso sarà utilizzato l'ultimo colore selezionato con il comando ''INK'' 
+oppure ''PEN'. Infine, lo stile della linea di default è pieno ma può essere impostata una 
+bitmask di 16 bit con il comando ''SET LINE''.
+
+@syntax DRAW { [x1] },{ [y1] } TO { [x2] },{ [x2] },{ [c] } 
+@syntax DRAW { [x1] },{ [x1] } TO { [x1] },{ [x1] } 
+@syntax DRAW TO { [x1] },{ [x1] },{ [c] } 
+@syntax DRAW TO { [x1] },{ [x1] } 
+
+@example DRAW 10,10 TO 100,100,WHITE
+@example DRAW TO 100,100
+@example DRAW ,10 TO ,100
+@usedInExample graphics_color_01.bas
+@usedInExample graphics_lines_01.bas
+@usedInExample graphics_shapes_01.bas
+@usedInExample graphics_shapes_02.bas
+@usedInExample graphics_shapes_03.bas
+
+@target all
+</usermanual> */
+
 void draw( Environment * _environment, char * _x0, char * _y0, char * _x1, char * _y1, char * _c ) {
 
     Variable * zero = variable_temporary( _environment, VT_POSITION, "(0)" );
@@ -55,218 +103,79 @@ void draw( Environment * _environment, char * _x0, char * _y0, char * _x1, char 
     Variable * x = variable_temporary( _environment, VT_POSITION, "(x)" );
     Variable * y = variable_temporary( _environment, VT_POSITION, "(y)" );
 
-    // int dx, dy;
     Variable * dx = variable_temporary( _environment, VT_POSITION, "(dx)");
     Variable * dy = variable_temporary( _environment, VT_POSITION, "(dy)");
     Variable * dx2 = variable_temporary( _environment, VT_POSITION, "(dx2)");
     Variable * dy2 = variable_temporary( _environment, VT_POSITION, "(dy2)");
 
-    // int stepx, stepy;
     Variable * stepx = variable_temporary( _environment, VT_POSITION, "(stepx)");
     Variable * stepy = variable_temporary( _environment, VT_POSITION, "(stepy)");
 
     variable_move( _environment, x0->name, x->name );
     variable_move( _environment, y0->name, y->name );
 
-    // dx = x1 - x0;
     variable_move( _environment, variable_sub( _environment, x1->name, x0->name )->name, dx->name );
     
-    // dy = y1 - y0;
     variable_move( _environment, variable_sub( _environment, y1->name, y0->name )->name, dy->name );
 
-    // if (dy < 0)
-    // {
     if_then( _environment, variable_less_than( _environment, dy->name, zero->name, 0 )->name );
-    //     dy = -dy;
         variable_move( _environment, variable_sub( _environment, zero->name, dy->name )->name, dy->name );
-    //     stepy = -1;
         variable_store( _environment, stepy->name, -1 );
-    // }
     else_if_then( _environment, NULL );
-    // else
-    // {
-    //     stepy = 1;
-        variable_store( _environment, stepy->name, 1 );
-    // }
+       variable_store( _environment, stepy->name, 1 );
     end_if_then( _environment );
-    // if (dx < 0)
-    // {
     if_then( _environment, variable_less_than( _environment, dx->name, zero->name, 0 )->name );
-    //     dx = -dx;
         variable_move( _environment, variable_sub( _environment, zero->name, dx->name )->name, dx->name );
-    //     stepx = -1;
         variable_store( _environment, stepx->name, -1 );
-    // }
     else_if_then( _environment, NULL );
-    // else
-    // {
-    //     stepx = 1;
         variable_store( _environment, stepx->name, 1 );
-    // }
     end_if_then( _environment );
-    // dy <<= 1; /* dy is now 2*dy */
     variable_move_naked( _environment, dy->name, dy2->name );
     variable_mul2_const( _environment, dy2->name, 1 );
-    // dx <<= 1; /* dx is now 2*dx */
     variable_move_naked( _environment, dx->name, dx2->name );
     variable_mul2_const( _environment, dx2->name, 1 );
-    // if ((0 <= x0) && (x0 < RDim) && (0 <= y0) && (y0 < RDim))
-    //     theRaster[x0][y0] = 1;
     if_then( _environment, variable_bit( _environment, pattern->name, bit->name )->name );
-        // cout << "(" << x << "," << y << ")\n";
         plot( _environment, x->name, y->name, _c );
     end_if_then( _environment );
     variable_increment( _environment, bit->name );
     if_then( _environment, variable_compare( _environment, bit->name, sixteen->name )->name );
         variable_store( _environment, bit->name, 0 );
     end_if_then( _environment );
-    // 16 if (dx > dy)
-    // {
     if_then( _environment, variable_greater_than( _environment, dx2->name, dy2->name, 0 )->name );
-    //     int fraction = dy - (dx >> 1);
         variable_move( _environment, variable_sub( _environment, dy2->name, dx->name)->name, fraction->name);
-    //     while (x0 != x1)
-    //     {
         begin_while( _environment );
         begin_while_condition( _environment, variable_compare_not( _environment, x->name, x1->name )->name );
-    //         x0 += stepx;
             variable_move( _environment, variable_add( _environment, x->name, stepx->name )->name, x->name );
-    //         if (fraction >= 0)
-    //         {
             if_then( _environment, variable_greater_than( _environment, fraction->name, zero->name, 1 )->name );
-    //             y0 += stepy;
                 variable_move( _environment, variable_add( _environment, y->name, stepy->name )->name, y->name );
-    //             fraction -= dx;
                 variable_move( _environment, variable_sub( _environment, fraction->name, dx2->name )->name, fraction->name );
-    //         }
             end_if_then( _environment );
-    //         fraction += dy;
             variable_move( _environment, variable_add( _environment, fraction->name, dy2->name )->name, fraction->name );
-    //         if ((0 <= x0) && (x0 < RDim) && (0 <= y0) && (y0 < RDim))
-    //             theRaster[x0][y0] = 1;
             if_then( _environment, variable_bit( _environment, pattern->name, bit->name )->name );
-                // cout << "(" << x << "," << y << ")\n";
                 plot( _environment, x->name, y->name, _c );
             end_if_then( _environment );
             variable_increment( _environment, bit->name );
             if_then( _environment, variable_compare( _environment, bit->name, sixteen->name )->name );
                 variable_store( _environment, bit->name, 0 );
             end_if_then( _environment );
-    //     }
         end_while( _environment );
-    // }
     else_if_then( _environment, NULL );
-    // else
-    // {
-    //     int fraction = dx - (dy >> 1);
         variable_move( _environment, variable_sub( _environment, dx2->name, dy->name)->name, fraction->name);
-    //     while (y0 != y1)
-    //     {
         begin_while( _environment );
         begin_while_condition( _environment, variable_compare_not( _environment, y->name, y1->name )->name );
-    //         if (fraction >= 0)
-    //         {
             if_then( _environment, variable_greater_than( _environment, fraction->name, zero->name, 1 )->name );
-    //             17 x0 += stepx;
                 variable_move( _environment, variable_add( _environment, x->name, stepx->name )->name, x->name );
-    //             fraction -= dy;
                 variable_move( _environment, variable_sub( _environment, fraction->name, dy2->name )->name, fraction->name );
-    //         }
             end_if_then( _environment );
-    //         y0 += stepy;
             variable_move( _environment, variable_add( _environment, y->name, stepy->name )->name, y->name );
-    //         fraction += dx;
             variable_move( _environment, variable_add( _environment, fraction->name, dx2->name )->name, fraction->name );
-    //         if ((0 <= x0) && (x0 < RDim) && (0 <= y0) && (y0 < RDim))
-    //             theRaster[x0][y0] = 1;
             if_then( _environment, variable_bit( _environment, pattern->name, bit->name )->name );
-                // cout << "(" << x << "," << y << ")\n";
                 plot( _environment, x->name, y->name, _c );
             end_if_then( _environment );
             variable_increment( _environment, bit->name );
             if_then( _environment, variable_compare( _environment, bit->name, sixteen->name )->name );
                 variable_store( _environment, bit->name, 0 );
             end_if_then( _environment );
-    //     }
         end_while( _environment );
-    // }
-    end_if_then( _environment );
-
-
-
-/*
-    Variable * x1 = variable_retrieve_or_define( _environment, _x1, VT_POSITION, 0 );
-    Variable * y1 = variable_retrieve_or_define( _environment, _y1, VT_POSITION, 0 );
-    Variable * x2 = variable_retrieve_or_define( _environment, _x2, VT_POSITION, 0 );
-    Variable * y2 = variable_retrieve_or_define( _environment, _y2, VT_POSITION, 0 );
-    Variable * pattern = variable_retrieve( _environment, "LINE" );
-    Variable * bit = variable_temporary( _environment, VT_BYTE, "(bit)" );
-    Variable * m_new = variable_temporary( _environment, VT_POSITION, "(m_new)" );
-    Variable * slope_error_new = variable_temporary( _environment, VT_POSITION, "(slope_error_new)" );
-    Variable * x = variable_temporary( _environment, VT_POSITION, "(x)" );
-    Variable * y = variable_temporary( _environment, VT_POSITION, "(y)" );
-    Variable * zero = variable_temporary( _environment, VT_POSITION, "(0)" );
-    Variable * sixteen = variable_temporary( _environment, VT_BYTE, "(16)" );
-
-    variable_store( _environment, zero->name, 0 );
-    variable_store( _environment, bit->name, 0 );
-    variable_store( _environment, sixteen->name, 16 );
-
-    if_then( _environment, variable_compare( _environment, x1->name, x2->name )->name );
-        variable_move( _environment, x1->name, x->name );
-        begin_for( _environment, y->name, y1->name, y2->name );
-
-            if_then( _environment, variable_bit( _environment, pattern->name, bit->name )->name );
-                // cout << "(" << x << "," << y << ")\n";
-                plot( _environment, x->name, y->name, _c );
-            end_if_then( _environment );
-
-            variable_increment( _environment, bit->name );
-            if_then( _environment, variable_compare( _environment, bit->name, sixteen->name )->name );
-                variable_store( _environment, bit->name, 0 );
-            end_if_then( _environment );
-
-        end_for( _environment );
-    else_if_then( _environment, NULL );
-
-        // int m_new = 2 * (y2 - y1);
-        variable_move( _environment, variable_mul2_const( _environment, variable_sub( _environment, y2->name, y1->name)->name , 1 )->name, m_new->name );
-
-        // int slope_error_new = m_new - (x2 - x1);
-        variable_move( _environment, variable_sub( _environment, m_new->name, variable_sub( _environment, x2->name, x1->name)->name )->name, slope_error_new->name );
-
-    // for (int x = x1, y = y1; x <= x2; x++)
-    //   {
-        variable_move( _environment, y1->name, y->name );
-        begin_for( _environment, x->name, x1->name, x2->name );
-
-            if_then( _environment, variable_bit( _environment, pattern->name, bit->name )->name );
-                // cout << "(" << x << "," << y << ")\n";
-                plot( _environment, x->name, y->name, _c );
-            end_if_then( _environment );
-
-            variable_increment( _environment, bit->name );
-            if_then( _environment, variable_compare( _environment, bit->name, sixteen->name )->name );
-                variable_store( _environment, bit->name, 0 );
-            end_if_then( _environment );
-
-            // Add slope to increment angle formed
-            // slope_error_new += m_new;
-            variable_move( _environment, variable_add( _environment, slope_error_new->name, m_new->name)->name, slope_error_new->name );
-
-            // Slope error reached limit, time to
-            // increment y and update slope error.
-            // if (slope_error_new >= 0)
-            // {
-            if_then( _environment, variable_greater_than( _environment, slope_error_new->name, zero->name, 1 )->name );
-                // y++;
-                variable_increment( _environment, y->name );
-                // slope_error_new  -= 2 * (x2 - x1);
-                variable_move( _environment, variable_sub( _environment, slope_error_new->name, variable_mul2_const( _environment, variable_sub( _environment, x2->name, x1->name )->name, 1 )->name )->name, slope_error_new->name );
-            // }
-            end_if_then( _environment );
-        // }
-        end_for( _environment );
-    end_if_then( _environment );
-*/
+   end_if_then( _environment );
 }
