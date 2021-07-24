0c704efcca69510f7e648206d73dafd3a79aca0a
spotlessmind1975 <asimov@mclink.it>
2021-07-24 09:03:28 +0000

    Added missing file for CLINE support for atari.

---
+++ ugbc/src/hw/gtia/cline.asm
@@ -0,0 +1,156 @@
+; /*****************************************************************************
+;  * ugBASIC - an isomorphic BASIC language compiler for retrocomputers        *
+;  *****************************************************************************
+;  * Copyright 2021 Marco Spedaletti (asimov@mclink.it)
+;  *
+;  * Licensed under the Apache License, Version 2.0 (the "License");
+;  * you may not use this file except in compliance with the License.
+;  * You may obtain a copy of the License at
+;  *
+;  * http://www.apache.org/licenses/LICENSE-2.0
+;  *
+;  * Unless required by applicable law or agreed to in writing, software
+;  * distributed under the License is distributed on an "AS IS" BASIS,
+;  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;  * See the License for the specific language governing permissions and
+;  * limitations under the License.
+;  *----------------------------------------------------------------------------
+;  * Concesso in licenza secondo i termini della Licenza Apache, versione 2.0
+;  * (la "Licenza"); è proibito usare questo file se non in conformità alla
+;  * Licenza. Una copia della Licenza è disponibile all'indirizzo:
+;  *
+;  * http://www.apache.org/licenses/LICENSE-2.0
+;  *
+;  * Se non richiesto dalla legislazione vigente o concordato per iscritto,
+;  * il software distribuito nei termini della Licenza è distribuito
+;  * "COSì COM'è", SENZA GARANZIE O CONDIZIONI DI ALCUN TIPO, esplicite o
+;  * implicite. Consultare la Licenza per il testo specifico che regola le
+;  * autorizzazioni e le limitazioni previste dalla medesima.
+;  ****************************************************************************/
+;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;*                                                                             *
+;*                      CLEAR LINE ROUTINE FOR GTIA                            *
+;*                                                                             *
+;*                             by Marco Spedaletti                             *
+;*                                                                             *
+;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+CLINE:
+    LDA CURRENTMODE
+    CMP #2
+    BNE CLINEANTIC2X
+    JMP CLINEANTIC2
+CLINEANTIC2X:
+    ; CMP #6
+    ; BEQ TEXTATANTIC6
+    ; CMP #7
+    ; BEQ TEXTATANTIC7
+    ; CMP #3
+    ; BEQ TEXTATANTIC3
+    ; CMP #4
+    ; BEQ TEXTATANTIC4
+    ; CMP #5
+    ; BEQ TEXTATANTIC5
+
+;     CMP #8
+;     BNE TEXTATANTIC8X
+;     JMP TEXTATANTIC8
+; TEXTATANTIC8X:
+    ; CMP #9
+    ; BEQ PLOTANTIC9
+    ; CMP #10
+    ; BEQ PLOTANTIC10
+    ; CMP #11
+    ; BEQ PLOTANTIC11
+    ; CMP #13
+    ; BEQ PLOTANTIC13
+    ; CMP #15
+    ; BEQ PLOTANTIC15
+    ; CMP #12
+    ; BEQ PLOTANTIC12
+    ; CMP #14
+    ; BEQ PLOTANTIC14
+    RTS
+
+CLINEANTIC2:
+    LDA TEXTADDRESS
+    STA COPYOFTEXTADDRESS
+    LDA TEXTADDRESS+1
+    STA COPYOFTEXTADDRESS+1
+
+    LDX CLINEY
+    BEQ CLINEANTIC2SKIP
+CLINEANTIC2L1:
+    CLC
+    LDA CURRENTWIDTH
+    ADC COPYOFTEXTADDRESS
+    STA COPYOFTEXTADDRESS
+    LDA #0
+    ADC COPYOFTEXTADDRESS+1
+    STA COPYOFTEXTADDRESS+1
+
+    DEX
+    BNE CLINEANTIC2L1
+
+CLINEANTIC2SKIP:
+    LDA CHARACTERS
+    BEQ CLINEANTIC2ENTIRE
+
+    CLC
+    LDA CLINEX
+    ADC COPYOFTEXTADDRESS
+    STA COPYOFTEXTADDRESS
+    LDA #0
+    ADC COPYOFTEXTADDRESS+1
+    STA COPYOFTEXTADDRESS+1
+
+    LDX CHARACTERS
+    LDY #0
+
+CLINEANTIC2INCX:
+    LDA #0
+    STA (COPYOFTEXTADDRESS),y
+        
+    INC CLINEX
+    LDA CLINEX
+    CMP CURRENTWIDTH
+    BNE CLINEANTIC2NEXT
+    LDA #0
+    STA CLINEX
+    INC CLINEY
+    LDA CLINEY
+    CMP CURRENTHEIGHT
+    BNE CLINEANTIC2NEXT
+
+    LDA #$FE
+    STA DIRECTION
+    JSR VSCROLLT
+
+    DEC CLINEY
+    SEC
+    LDA COPYOFTEXTADDRESS
+    SBC CURRENTWIDTH 
+    STA COPYOFTEXTADDRESS
+    LDA COPYOFTEXTADDRESS+1
+    SBC #0
+    STA COPYOFTEXTADDRESS+1
+ 
+CLINEANTIC2NEXT:
+    INY
+    DEX
+    BNE CLINEANTIC2INCX
+    RTS
+
+CLINEANTIC2ENTIRE:
+    LDY #0
+
+CLINEANTIC2INC2X:
+    LDA #0
+    STA (COPYOFTEXTADDRESS),y
+        
+    INY
+    INC CLINEX
+    LDA CLINEX
+    CMP CURRENTWIDTH
+    BNE CLINEANTIC2INC2X
+    RTS
---
+++ ugbc/src/hw/vic2/cline.asm
@@ -0,0 +1,151 @@
+; /*****************************************************************************
+;  * ugBASIC - an isomorphic BASIC language compiler for retrocomputers        *
+;  *****************************************************************************
+;  * Copyright 2021 Marco Spedaletti (asimov@mclink.it)
+;  *
+;  * Licensed under the Apache License, Version 2.0 (the "License");
+;  * you may not use this file except in compliance with the License.
+;  * You may obtain a copy of the License at
+;  *
+;  * http://www.apache.org/licenses/LICENSE-2.0
+;  *
+;  * Unless required by applicable law or agreed to in writing, software
+;  * distributed under the License is distributed on an "AS IS" BASIS,
+;  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;  * See the License for the specific language governing permissions and
+;  * limitations under the License.
+;  *----------------------------------------------------------------------------
+;  * Concesso in licenza secondo i termini della Licenza Apache, versione 2.0
+;  * (la "Licenza"); è proibito usare questo file se non in conformità alla
+;  * Licenza. Una copia della Licenza è disponibile all'indirizzo:
+;  *
+;  * http://www.apache.org/licenses/LICENSE-2.0
+;  *
+;  * Se non richiesto dalla legislazione vigente o concordato per iscritto,
+;  * il software distribuito nei termini della Licenza è distribuito
+;  * "COSì COM'è", SENZA GARANZIE O CONDIZIONI DI ALCUN TIPO, esplicite o
+;  * implicite. Consultare la Licenza per il testo specifico che regola le
+;  * autorizzazioni e le limitazioni previste dalla medesima.
+;  ****************************************************************************/
+;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;*                                                                             *
+;*                      CLEAR LINE ROUTINE FOR VIC-II                          *
+;*                                                                             *
+;*                             by Marco Spedaletti                             *
+;*                                                                             *
+;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+CLINE:
+    LDA TEXTADDRESS
+    STA COPYOFTEXTADDRESS
+    LDA TEXTADDRESS+1
+    STA COPYOFTEXTADDRESS+1
+    LDA COLORMAPADDRESS
+    STA COPYOFCOLORMAPADDRESS
+    LDA COLORMAPADDRESS+1
+    STA COPYOFCOLORMAPADDRESS+1
+
+    LDX CLINEY
+    BEQ CLINESKIP
+CLINEL1:
+    CLC
+    LDA #40
+    ADC COPYOFTEXTADDRESS
+    STA COPYOFTEXTADDRESS
+    LDA #0
+    ADC COPYOFTEXTADDRESS+1
+    STA COPYOFTEXTADDRESS+1
+
+    DEX
+    BNE CLINEL1
+
+    LDX CLINEY
+CLINELC1:
+    CLC
+    LDA #40
+    ADC COPYOFCOLORMAPADDRESS
+    STA COPYOFCOLORMAPADDRESS
+    LDA #0
+    ADC COPYOFCOLORMAPADDRESS+1
+    STA COPYOFCOLORMAPADDRESS+1
+    DEX
+    BNE CLINELC1
+
+CLINESKIP:
+    LDA CHARACTERS
+    BEQ CLINEENTIRE
+
+    CLC
+    LDA CLINEX
+    ADC COPYOFTEXTADDRESS
+    STA COPYOFTEXTADDRESS
+    LDA #0
+    ADC COPYOFTEXTADDRESS+1
+    STA COPYOFTEXTADDRESS+1
+
+    CLC
+    LDA CLINEX
+    ADC COPYOFCOLORMAPADDRESS
+    STA COPYOFCOLORMAPADDRESS
+    LDA #0
+    ADC COPYOFCOLORMAPADDRESS+1
+    STA COPYOFCOLORMAPADDRESS+1
+
+    LDX CHARACTERS
+    LDY #0
+
+CLINEINCX:
+    LDA #32
+    STA (COPYOFTEXTADDRESS),y
+        
+    INC CLINEX
+    LDA CLINEX
+    CMP #40
+    BNE CLINENEXT
+    LDA #0
+    STA CLINEX
+    INC CLINEY
+    LDA CLINEY
+    CMP #24
+    BNE CLINENEXT
+
+    LDA #$FE
+    STA DIRECTION
+    JSR VSCROLLT
+
+    DEC CLINEY
+    SEC
+    LDA COPYOFTEXTADDRESS
+    SBC #40 
+    STA COPYOFTEXTADDRESS
+    LDA COPYOFTEXTADDRESS+1
+    SBC #0
+    STA COPYOFTEXTADDRESS+1
+
+    SEC
+    LDA COPYOFCOLORMAPADDRESS
+    SBC #40
+    STA COPYOFCOLORMAPADDRESS
+    LDA COPYOFCOLORMAPADDRESS+1
+    SBC #0
+    STA COPYOFCOLORMAPADDRESS+1
+ 
+CLINENEXT:
+    INY
+    DEX
+    BNE CLINEINCX
+    RTS
+
+CLINEENTIRE:
+    LDY #0
+
+CLINEINC2X:
+    LDA #32
+    STA (COPYOFTEXTADDRESS),y
+        
+    INY
+    INC CLINEX
+    LDA CLINEX
+    CMP #40
+    BNE CLINEINC2X
+    RTS
