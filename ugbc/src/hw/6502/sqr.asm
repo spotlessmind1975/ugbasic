de9176dc7ac2bd5c2b7d4fb0186a9508d5f355db
spotlessmind1975 <asimov@mclink.it>
2021-08-10 13:47:35 +0000

    Added support for SQR function (6502).

---
+++ ugbc/src/hw/6502/sqr.asm
@@ -0,0 +1,80 @@
+; Square Root Calculator by Lee Davison
+;
+; How to calculate the 8-bit unsigned integer square root of an unsigned 16-bit integer.
+; By Lee Davison (leeedavison@googlemail.com), 23 July 2001.
+; Calculates the 8 bit root and 9 bit remainder of a 16 bit unsigned integer in
+; Numberl/Numberh. The result is always in the range 0 to 255 and is held in
+; Root, the remainder is in the range 0 to 511 and is held in Reml/Remh
+;
+; partial results are held in templ/temph
+;
+; This routine is the complement to the integer square program.
+;
+; Destroys A, X registers.
+;
+; variables - must be in RAM
+; Originally published on http://6502.org/source/integers/root.htm 
+; Adapted by Marco Spedaletti for ugBASIC   
+
+Numberl		= $F0		; number to find square root of low byte
+Numberh		= Numberl+1	; number to find square root of high byte
+Reml		= $F2		; remainder low byte
+Remh		= Reml+1	; remainder high byte
+templ		= $F4		; temp partial low byte
+temph		= templ+1	; temp partial high byte
+Root		= $F6		; square root
+
+SQROOT
+	LDA	#$00		; clear A
+	STA	Reml		; clear remainder low byte
+	STA	Remh		; clear remainder high byte
+	STA	Root		; clear Root
+	LDX	#$08		; 8 pairs of bits to do
+SQROOTL1
+	ASL	Root		; Root = Root * 2
+
+	ASL	Numberl		; shift highest bit of number ..
+	ROL	Numberh		;
+	ROL	Reml		; .. into remainder
+	ROL	Remh		;
+
+	ASL	Numberl		; shift highest bit of number ..
+	ROL	Numberh		;
+	ROL	Reml		; .. into remainder
+	ROL	Remh		;
+
+	LDA	Root		; copy Root ..
+	STA	templ		; .. to templ
+	LDA	#$00		; clear byte
+	STA	temph		; clear temp high byte
+
+	SEC			; +1
+	ROL	templ		; temp = temp * 2 + 1
+	ROL	temph		;
+
+	LDA	Remh		; get remainder high byte
+	CMP	temph		; comapre with partial high byte
+	BCC	SQROOTNX		; skip sub if remainder high byte smaller
+
+	BNE	SQROOTSB		; do sub if <> (must be remainder>partial !)
+
+	LDA	Reml		; get remainder low byte
+	CMP	templ		; comapre with partial low byte
+	BCC	SQROOTNX		; skip sub if remainder low byte smaller
+
+				; else remainder>=partial so subtract then
+				; and add 1 to root. carry is always set here
+SQROOTSB
+	LDA	Reml		; get remainder low byte
+	SBC	templ		; subtract partial low byte
+	STA	Reml		; save remainder low byte
+	LDA	Remh		; get remainder high byte
+	SBC	temph		; subtract partial high byte
+	STA	Remh		; save remainder high byte
+
+	INC	Root		; increment Root
+SQROOTNX
+	DEX			; decrement bit pair count
+	BNE	SQROOTL1		; loop if not all done
+
+	RTS
---
+++ ugbc/src/targets/common/sqr.c
@@ -0,0 +1,92 @@
+/*****************************************************************************
+ * ugBASIC - an isomorphic BASIC language compiler for retrocomputers        *
+ *****************************************************************************
+ * Copyright 2021 Marco Spedaletti (asimov@mclink.it)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *----------------------------------------------------------------------------
+ * Concesso in licenza secondo i termini della Licenza Apache, versione 2.0
+ * (la "Licenza"); è proibito usare questo file se non in conformità alla
+ * Licenza. Una copia della Licenza è disponibile all'indirizzo:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Se non richiesto dalla legislazione vigente o concordato per iscritto,
+ * il software distribuito nei termini della Licenza è distribuito
+ * "COSÌ COM'È", SENZA GARANZIE O CONDIZIONI DI ALCUN TIPO, esplicite o
+ * implicite. Consultare la Licenza per il testo specifico che regola le
+ * autorizzazioni e le limitazioni previste dalla medesima.
+ ****************************************************************************/
+
+/****************************************************************************
+ * INCLUDE SECTION 
+ ****************************************************************************/
+
+#include "../../ugbc.h"
+
+/****************************************************************************
+ * CODE SECTION 
+ ****************************************************************************/
+
+extern char DATATYPE_AS_STRING[][16];
+
+/**
+ * @brief Return the square root of a variable
+ * 
+ * @param _environment Current calling environment
+ * @param _value Value to calculate the square root
+ * @return Variable* The square root of value
+ */
+/* <usermanual>
+@keyword SQR
+
+@english
+The ''SQR'' function returns a value representing the square root of a number.
+
+@italian
+La funzione ''SQR'' restituisce un valore che rappresenta la radice quadrata di un numero.
+
+@syntax SQR([expression])
+
+@example x = SNG(81)
+
+@usedInExample maths_sqr_01.bas
+
+@target all
+</usermanual> */
+
+Variable * sqroot( Environment * _environment, char * _value ) {
+    Variable * value = variable_retrieve( _environment, _value );
+
+    Variable * result = variable_temporary( _environment, VT_BYTE, "(result of SQR)");
+
+    switch( VT_BITWIDTH( value->type ) ) {
+        case 32:
+            CRITICAL_SQR_UNSUPPORTED( _value, DATATYPE_AS_STRING[value->type] );
+        case 16:
+            if ( VT_SIGNED( value->type ) ) {
+                CRITICAL_SQR_UNSUPPORTED( _value, DATATYPE_AS_STRING[value->type] );
+            }
+            cpu_sqroot( _environment, value->realName, result->realName );
+            break;
+        case 8:
+            CRITICAL_SQR_UNSUPPORTED( _value, DATATYPE_AS_STRING[value->type] );
+            break;
+        case 0:
+            CRITICAL_SGN_UNSUPPORTED( _value, DATATYPE_AS_STRING[value->type] );
+            break;
+    }
+
+    return result;
+}
+
