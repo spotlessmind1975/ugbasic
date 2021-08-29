67b94539d72ad93654475b392313ca218e7a710a
spotlessmind1975 <asimov@mclink.it>
2021-08-29 10:42:41 +0000

    Add support for listing on c64 testing.

--- ugbc/src-test/tester_c64.c
+++ ugbc/src-test/tester_c64.c
@@ -62,8 +62,8 @@ void create_test( char *_name, void (*_payload)(TestEnvironment *), int (*_teste
     }
     fclose(handleIns);
     
-    system("cl65 -Ln /tmp/out.lbl -g -o /tmp/out.prg -C /tmp/out.cfg -u __EXEHDR__ -t c64 /tmp/out.asm");
-    system("run6502 -L /tmp/out.lbl -Li /tmp/out.ins -X 0000 -R 080d -l 07ff /tmp/out.prg -O /tmp/out.out");
+    system("cl65 -l /tmp/out.lis -Ln /tmp/out.lbl -g -o /tmp/out.prg -C /tmp/out.cfg -u __EXEHDR__ -t c64 /tmp/out.asm");
+    system("run6502 -L /tmp/out.lbl -Li /tmp/out.ins -X 0000 -R 080d -l 07ff /tmp/out.prg -O /tmp/out.out -u /tmp/out.lis");
     FILE * handle = fopen( "/tmp/out.out", "rt" );
     fscanf(handle, "%x %x %x %x %x %x", &t.state.a, &t.state.x, &t.state.y, &t.state.p, &t.state.s, &t.state.pc );
     while( !feof(handle) ) {
