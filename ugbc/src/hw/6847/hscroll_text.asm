0567879e3f5efb49be0e746a86c4065e1267252b
spotlessmind1975 <asimov@mclink.it>
2021-09-15 05:51:06 +0000

    Add support for Dragon 32 #121 - CLINE

--- ugbc/src/hw/6847/hscroll_text.asm
+++ ugbc/src/hw/6847/hscroll_text.asm
@@ -7,7 +7,7 @@
 ;  * you may not use this file except in compliance with the License.
 ;  * You may obtain a copy of the License at
 ;  *
-;  * http//www.apache.org/licenses/LICENSE-2.0
+;  * http://www.apache.org/licenses/LICENSE-2.0
 ;  *
 ;  * Unless required by applicable law or agreed to in writing, software
 ;  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,9 +17,9 @@
 ;  *----------------------------------------------------------------------------
 ;  * Concesso in licenza secondo i termini della Licenza Apache, versione 2.0
 ;  * (la "Licenza"); è proibito usare questo file se non in conformità alla
-;  * Licenza. Una copia della Licenza è disponibile all'indirizzo
+;  * Licenza. Una copia della Licenza è disponibile all'indirizzo:
 ;  *
-;  * http//www.apache.org/licenses/LICENSE-2.0
+;  * http://www.apache.org/licenses/LICENSE-2.0
 ;  *
 ;  * Se non richiesto dalla legislazione vigente o concordato per iscritto,
 ;  * il software distribuito nei termini della Licenza è distribuito
@@ -29,71 +29,130 @@
 ;  ****************************************************************************/
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 ;*                                                                             *
-;*                       CLEAR LINE ROUTINE FOR 6847                           *
+;*                          HIRIZONTAL SCROLL ON 6847                          *
 ;*                                                                             *
 ;*                             by Marco Spedaletti                             *
 ;*                                                                             *
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-CLINE
-    LDA CURRENTMODE
-    CMPA #2
-    BHI CLINE0X
-    JMP CLINET
-CLINE0X
-    RTS
-
-CLINET
+HSCROLLST
     LDX TEXTADDRESS
+ 
+    LDU CURRENTHEIGHT
+    LEAU 1,U
+
+    TFR X, Y
+    LEAY 1, Y
+
+HSCROLLSTL1
+    LDA DIRECTION
+    CMPA #$00
+    BGT HSCROLLSTRIGHT
+
+HSCROLLSTLEFT
+    LDB #0
+HSCROLLSTLEFTL1
+    LDA B,Y
+    STA B,X
+    INCB
+    CMPB CURRENTWIDTH+1
+    BNE HSCROLLSTLEFTL1
+    DECB
+    DECB
+    LDA EMPTYTILE
+    STA B,Y
 
     LDA CURRENTWIDTH+1
+    LEAX A, X
+    LEAY A, Y
+
+    LEAU -1, U
+    CMPU #0
+    BNE HSCROLLSTLEFT
+    RTS
+
+HSCROLLSTRIGHT
+    LDB CURRENTWIDTH+1
+HSCROLLSTRIGHTL1
+    LDA B,X
+    STA B,Y
+    DECB
+    CMPB #0
+    BNE HSCROLLSTRIGHTL1
+    LDA EMPTYTILE
+    STA B,Y
+
+    LDA CURRENTWIDTH+1
+    LEAX A, X
+    LEAY A, Y
+
+    LEAU -1, U
+    CMPU #0
+    BNE HSCROLLSTRIGHT
+    RTS
+
+HSCROLLLT
+    LDA DIRECTION
+    CMPA #$0
+    BGT HSCROLLLRIGHT
+
+HSCROLLLLEFT
+    LDX TEXTADDRESS
+    LDA CURRENTWIDTH+1
+
     LDB CLINEY
     CMPB #0
-    BEQ CLINESKIP
-CLINEL1
-    ANDCC #$FE
+    BEQ HSCROLLLLEFTTLS
+HSCROLLLTLL1
     LEAX A, X
     DECB
-    CMPB #0
-    BNE CLINEL1
+    CMPB #$0
+    BNE HSCROLLLTLL1
 
-CLINESKIP
-    LDA CHARACTERS
-    CMPA #0
-    BEQ CLINEENTIRE
+HSCROLLLLEFTTLS
+    TFR X, Y
+    LEAY 1, Y
 
-    ANDCC #$FE
-    LDA CLINEX
-    LEAX A, X
-
-    LDB CHARACTERS
-    LDU #0
-
-CLINEINCX
-    LDA EMPTYTILE
-    STA ,X
-    LEAX 1, X    
-    INC CLINEX
-    DECB
-    CMPB #0
-    BEQ CLINEDONE
-    LDA CLINEX
-    CMPA CURRENTWIDTH+1
-    BNE CLINEINCX
-     
-CLINEDONE
-    RTS
-
-CLINEENTIRE
-    LDB CLINEX
-
-CLINEINC2X
-    LDA EMPTYTILE
-    STA B, X
-        
+    LDB #0
+HSCROLLLLEFTL1
+    LDA B,Y
+    STA B,X
     INCB
-    INC CLINEX
-    LDB CLINEX
     CMPB CURRENTWIDTH+1
-    BNE CLINEINC2X
+    BNE HSCROLLLLEFTL1
+    DECB
+    LDA EMPTYTILE
+    STA B,X
     RTS
+
+
+HSCROLLLRIGHT
+    LDX TEXTADDRESS
+    LDA CURRENTWIDTH+1
+
+    LDB CLINEY
+    CMPB #0
+    BEQ HSCROLLLRIGHTTLS
+HSCROLLLTLL1B
+    LEAX A, X
+    DECB
+    CMPB #$0
+    BNE HSCROLLLTLL1B
+
+HSCROLLLRIGHTTLS
+    TFR X, Y
+    LEAY 1, Y
+    LDB CURRENTWIDTH+1
+    DECB
+    DECB
+HSCROLLLRIGHTL1
+    LDA B,X
+    STA B,Y
+    DECB
+    CMPB #$FF
+    BNE HSCROLLLRIGHTL1
+    INCB
+    LDA EMPTYTILE
+    STA B,X
+    RTS
+    
