e8de8d90ce09f24b74283b6822b66ca499b8d60b
spotlessmind1975 <asimov@mclink.it>
2021-09-26 16:09:50 +0000

    Fix bug on 16 bit decrement (6502).

--- ugbc/src/hw/6502.c
+++ ugbc/src/hw/6502.c
@@ -2815,6 +2815,7 @@ void cpu6502_dec_16bit( Environment * _environment, char * _variable ) {
     inline( cpu_dec_16bit )
 
         outline1("DEC %s", _variable );
+        outline0("CMP #$FF" );
         outline1("BNE %s", label );
         outline1("DEC %s+1", _variable );
         outhead1("%s:", label );
