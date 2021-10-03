d8b17d0bfd6d25ec196275dc84d5b81cea3a6958
spotlessmind1975 <asimov@mclink.it>
2021-10-03 15:54:48 +0000

    Updated tests for new usim parameters (d32,d64).

--- ugbc/src-test/tester_d32.c
+++ ugbc/src-test/tester_d32.c
@@ -63,7 +63,7 @@ void create_test( char *_name, void (*_payload)(TestEnvironment *), int (*_teste
     fclose(handleIns);
 
     system("asm6809 -H -e 7168 /tmp/out.asm -o /tmp/out.hex -s /tmp/out.sym -l /tmp/out.lis");
-    system("usim -t -i /tmp/out.lis -R 1C00 -L2 /tmp/out.lb2 -L /tmp/out.sym -Li /tmp/out.ins -l 1C00 /tmp/out.hex -O /tmp/out.out");
+    system("usim -i /tmp/out.lis -R 1C00 -L2 /tmp/out.lb2 -L /tmp/out.sym -Li /tmp/out.ins -l 0000 /tmp/out.hex -O /tmp/out.out");
     FILE * handle = fopen( "/tmp/out.out", "rt" );
     fscanf(handle, "%x %x %x %x %x %x %x %x", 
     	&t.state.a,
