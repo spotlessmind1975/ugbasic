6151b0cbbc2c66283d1169520b48ff52c9f2ee6b
spotlessmind1975 <asimov@mclink.it>
2021-10-28 11:57:40 +0000

    Byteorder issue #173 - fixed

--- ugbc/src/targets/mo5/_cleanup.c
+++ ugbc/src/targets/mo5/_cleanup.c
@@ -92,12 +92,38 @@ int frmt( Environment * _environment, char * ptr, char size,char* out)
 		return 0;
 }
 
-void swapen(unsigned short *num)
-{
-unsigned short swapped;
-swapped = (*num>>8) | (*num<<8);
-*num=swapped;
-}
+// FIX #173
+#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN || \
+    defined(__BIG_ENDIAN__) || \
+    defined(__ARMEB__) || \
+    defined(__THUMBEB__) || \
+    defined(__AARCH64EB__) || \
+    defined(_MIBSEB) || defined(__MIBSEB) || defined(__MIBSEB__)
+
+	void swapen(unsigned short *num)
+	{
+		return *num;
+	}
+
+#elif defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN || \
+    defined(__LITTLE_ENDIAN__) || \
+    defined(__ARMEL__) || \
+    defined(__THUMBEL__) || \
+    defined(__AARCH64EL__) || \
+    defined(_MIPSEL) || defined(__MIPSEL) || defined(__MIPSEL__)
+
+	void swapen(unsigned short *num)
+	{
+	unsigned short swapped;
+	swapped = (*num>>8) | (*num<<8);
+	*num=swapped;
+	}
+
+#else
+
+	#error "Cannot define swapen() since the byte ordering is unknown!"
+	
+#endif
 
 void crc ( unsigned char * data, int l,unsigned int   * old)
 { 
