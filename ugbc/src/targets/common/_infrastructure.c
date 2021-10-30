43d64ef1da395ddf5808563977b2943da324b2cb
spotlessmind1975 <asimov@mclink.it>
2021-10-30 21:30:50 +0000

    Removed debug print.

--- ugbc/src/targets/common/_infrastructure.c
+++ ugbc/src/targets/common/_infrastructure.c
@@ -801,7 +801,6 @@ Variable * variable_store( Environment * _environment, char * _destination, unsi
                 } else {
                     CRITICAL_DATATYPE_UNSUPPORTED("array(1)", DATATYPE_AS_STRING[destination->arrayType]);
                 }
-                printf("Filling array with %d bytes of value %2.2x\n", size, _value );
                 cpu_fill_direct_size_value( _environment, destination->realName, size, _value );
             } else {
                 CRITICAL_STORE_UNSUPPORTED(DATATYPE_AS_STRING[destination->type]);
