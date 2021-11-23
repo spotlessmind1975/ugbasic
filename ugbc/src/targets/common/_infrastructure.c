21283cffd568e82e964ab9a02c86d5bcb6581043
spotlessmind1975 <asimov@mclink.it>
2021-11-23 23:00:01 +0000

    add_complex() unsigned ? #250 - fixed

--- ugbc/src/targets/common/_infrastructure.c
+++ ugbc/src/targets/common/_infrastructure.c
@@ -1675,18 +1675,18 @@ Variable * variable_div( Environment * _environment, char * _source, char * _des
     Variable * remainder = NULL;
     switch( VT_BITWIDTH( source->type ) ) {
         case 32:
-            result = variable_temporary( _environment, VT_DWORD, "(result of division)" );
-            remainder = variable_temporary( _environment, VT_WORD, "(remainder of division)" );
+            result = variable_temporary( _environment, VT_SIGNED( source->type ) ? VT_SWORD : VT_WORD, "(result of division)" );
+            remainder = variable_temporary( _environment, VT_SIGNED( source->type ) ? VT_SWORD : VT_WORD, "(remainder of division)" );
             cpu_math_div_32bit_to_16bit( _environment, source->realName, target->realName, result->realName, remainder->realName, VT_SIGNED( source->type ) );
             break;
         case 16:
-            result = variable_temporary( _environment, VT_WORD, "(result of division)" );
-            remainder = variable_temporary( _environment, VT_WORD, "(remainder of division)" );
+            result = variable_temporary( _environment, VT_SIGNED( source->type ) ? VT_SWORD : VT_WORD, "(result of division)" );
+            remainder = variable_temporary( _environment, VT_SIGNED( source->type ) ? VT_SWORD : VT_WORD, "(remainder of division)" );
             cpu_math_div_16bit_to_16bit( _environment, source->realName, target->realName, result->realName, remainder->realName, VT_SIGNED( source->type ) );
             break;
         case 8:
-            result = variable_temporary( _environment, VT_BYTE, "(result of division)" );
-            remainder = variable_temporary( _environment, VT_BYTE, "(remainder of division)" );
+            result = variable_temporary( _environment, VT_SIGNED( source->type ) ? VT_SBYTE : VT_BYTE, "(result of division)" );
+            remainder = variable_temporary( _environment, VT_SIGNED( source->type ) ? VT_SBYTE : VT_BYTE, "(remainder of division)" );
             cpu_math_div_8bit_to_8bit( _environment, source->realName, target->realName, result->realName, remainder->realName, VT_SIGNED( source->type ) );
             break;
         case 0:
