53f38e871aa6db87a071f9c4456a8a8fc600af83
spotlessmind1975 <asimov@mclink.it>
2021-12-16 12:46:31 +0000

    Fixed code generation (pc128op).

--- ugbc/src/targets/pc128op/_var.c
+++ ugbc/src/targets/pc128op/_var.c
@@ -92,8 +92,8 @@ static void variable_cleanup_entry( Environment * _environment, Variable * _firs
                         outhead2("%s equ $%4.4x", variable->realName, variable->absoluteAddress);
                     } else {
                         if ( variable->printable ) {
-                            c = strlen( variable->valueString );
-                            out1("%s fcb %d,", variable->realName, c );
+                            int c = strlen( variable->valueString );
+                            out2("%s fcb %d,", variable->realName, c );
                             int i=0;
                             for (i=0; i<(c-1); ++i ) {
                                 out1("$%2.2x,", (unsigned char)variable->valueString[i]);
