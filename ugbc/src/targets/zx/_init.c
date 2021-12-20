f3e0866942123c0f31cf5a1d022f44400037e782
spotlessmind1975 <asimov@mclink.it>
2021-12-20 18:05:16 +0000

    [pc128op/MO5]Defining a variable with a default value is broken #305.

--- ugbc/src/targets/zx/_init.c
+++ ugbc/src/targets/zx/_init.c
@@ -82,6 +82,8 @@ void target_initialization( Environment * _environment ) {
         shell_injection( _environment );
     }
 
+    cpu_call( _environment, "VARINIT" );
+
     variable_define( _environment, "bitmap_enabled", VT_BYTE, 0 );
     variable_global( _environment, "bitmap_enabled" );
     variable_define( _environment, "COLORMAPADDRESS", VT_ADDRESS, 0x5800 );
