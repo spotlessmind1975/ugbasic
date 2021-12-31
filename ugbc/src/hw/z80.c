2a052839bcf9f684e211b8f392fbc8e14a72f357
spotlessmind1975 <asimov@mclink.it>
2021-12-31 16:54:01 +0000

    Fixed z80_move_8bit_indirect_with_offset (z80).

--- ugbc/src/hw/z80.c
+++ ugbc/src/hw/z80.c
@@ -2845,7 +2845,7 @@ void z80_move_8bit_with_offset2( Environment * _environment, char *_source, char
 
 void z80_move_8bit_indirect_with_offset( Environment * _environment, char *_source, char * _value, int _offset ) {
 
-    outline1("LD HL, %s", _value);
+    outline1("LD HL, (%s)", _value);
     outline1("LD DE, $%2.2x", ( _offset & 0xff ) );
     outline0("ADD HL, DE" );
     outline1("LD A, (%s)", _source);
