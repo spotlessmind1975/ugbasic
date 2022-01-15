aa1feddae03812a9cd57da0883fe48563005fa78
spotlessmind1975 <asimov@mclink.it>
2022-01-15 16:50:51 +0000

    Added support for COLLISION (msx1, tms9918).

--- ugbc/src/targets/msx1/collision.c
+++ ugbc/src/targets/msx1/collision.c
@@ -56,12 +56,6 @@
 </usermanual> */
 Variable * collision_to( Environment * _environment, int _sprite ) {
 
-    Variable * result = variable_temporary( _environment, VT_BYTE, "(result)" );
-
-    char sprite[MAX_TEMPORARY_STORAGE]; sprintf(sprite, "$%2.2x", _sprite);
-
-    return result;
-
 }
 
 /**
@@ -79,13 +73,7 @@ Variable * collision_to( Environment * _environment, int _sprite ) {
  */
 Variable * collision_to_vars( Environment * _environment, char * _sprite ) {
 
-    // Safety check -- expression must exists (it should be always true)
-    Variable * sprite = variable_retrieve( _environment, _sprite );
-
-    // Safety check -- expression must exists (it should be always true)
-    Variable * result = variable_temporary( _environment, VT_BYTE, "(result)" );
-
-    return result;
+    return tms9918_collision( _environment, _sprite );
 
 }
 
