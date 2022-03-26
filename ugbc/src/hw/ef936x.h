8da820cd29596e5f01332514cbdb27ccc1ee9c3b
spotlessmind1975 <asimov@mclink.it>
2022-03-26 13:48:32 +0000

    Support for SEQUENCE for PUT IMAGE #379 - fix.

--- ugbc/src/hw/ef936x.h
+++ ugbc/src/hw/ef936x.h
@@ -196,7 +196,7 @@ void ef936x_scroll( Environment * _environment, int _dx, int _dy );
 Variable * ef936x_get_raster_line( Environment * _environment );
 
 Variable * ef936x_image_converter( Environment * _environment, char * _data, int _width, int _height, int _offset_x, int _offset_y, int _frame_width, int _frame_height, int _mode, int _transparent_color, int _flags );
-void ef936x_put_image( Environment * _environment, char * _image, char * _x, char * _y, char * _frame, int _frame_size, int _flags );
+void ef936x_put_image( Environment * _environment, char * _image, char * _x, char * _y, char * _frame, char * _sequence, int _frame_size, int _frame_count, int _flags );
 Variable * ef936x_new_image( Environment * _environment, int _width, int _height, int _mode );
 void ef936x_get_image( Environment * _environment, char * _image, char * _x, char * _y );
 
