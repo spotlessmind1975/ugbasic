37f856f8d334f8be65e6c0bc956ca203ac1e9b13
spotlessmind1975 <asimov@mclink.it>
2022-04-07 12:16:34 +0000

    Moved third part libraries on lib folder.

--- ugbc/src/targets/common/image_load.c
+++ ugbc/src/targets/common/image_load.c
@@ -34,7 +34,7 @@
 
 #include "../../ugbc.h"
 #define STB_IMAGE_IMPLEMENTATION
-#include "../../stb_image.h"
+#include "../../lib/stb_image.h"
 
 /****************************************************************************
  * CODE SECTION 
