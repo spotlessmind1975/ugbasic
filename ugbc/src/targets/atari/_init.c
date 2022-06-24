40d2ba2541e80d4a3f1fb73d41b314fe06d711fc
spotlessmind1975 <asimov@mclink.it>
2022-06-24 16:40:07 +0000

    Added support for profiling on atari / atarixl.

--- ugbc/src/targets/atari/_init.c
+++ ugbc/src/targets/atari/_init.c
@@ -157,6 +157,34 @@ void target_linkage( Environment * _environment ) {
         printf("Please use option '-I' to install chain tool.\n\n");
     }; 
 
+    if ( _environment->listingFileName ) {
+
+        if ( _environment->profileFileName ) {
+            if ( _environment->executerFileName ) {
+                sprintf(executableName, "%s", _environment->executerFileName );
+            } else if( access( "run6502.exe", F_OK ) == 0 ) {
+                sprintf(executableName, "%s", "run6502.exe" );
+            } else {
+                sprintf(executableName, "%s", "run6502" );
+            }
+
+            sprintf( commandLine, "\"%s\" -c atari -X 0000 -R 2000 -l 1ffa \"%s\" -u \"%s\" -p \"%s\" %d",
+                executableName,
+                _environment->exeFileName,
+                _environment->listingFileName,
+                _environment->profileFileName,
+                _environment->profileCycles ? _environment->profileCycles : 1000000
+                );
+
+            if ( system_call( _environment,  commandLine ) ) {
+                printf("The profiling of assembly program failed.\n\n");
+                return;
+            }; 
+
+        }
+    
+    }
+
 }
 
 void interleaved_instructions( Environment * _environment ) {
