a4ee56134b732915c21d8e88085466269ddf9930
spotlessmind1975 <asimov@mclink.it>
2022-07-05 22:52:34 +0000

    Add support for MSC1 compression / decompression of images #450

--- ugbc/src/targets/common/sequence_load.c
+++ ugbc/src/targets/common/sequence_load.c
@@ -34,6 +34,7 @@
 
 #include "../../ugbc.h"
 #include "../../stb_image.h"
+#include "../../libs/msc1.h"
 
 /****************************************************************************
  * CODE SECTION 
@@ -210,6 +211,35 @@ Variable * sequence_load( Environment * _environment, char * _filename, char * _
 
     if ( _bank_expansion && _environment->expansionBanks ) {
 
+        // Try to compress the result of image conversion.
+        // This means that the buffer will be compressed using MSC1
+        // algorithm, up to 32 frequent sequences. The original size of
+        // the buffer will be considered as "uncompressed" size.
+        MSC1Compressor * compressor = msc1_create( 32 );
+        result->uncompressedSize = result->size;
+        MemoryBlock * output = msc1_compress( compressor, result->valueBuffer, result->uncompressedSize, &result->size );
+
+        int temporary;
+        MemoryBlock * outputCheck = msc1_uncompress( compressor, output, result->size, &temporary );
+        if ( memcmp( outputCheck, result->valueBuffer, result->uncompressedSize ) != 0 ) {
+            CRITICAL("Compression failed");
+        }
+        msc1_free( compressor );
+        // If the compressed memory is greater than the original
+        // size, we discard the compression and we will continue as
+        // usual.
+        if ( result->uncompressedSize < result->size ) {
+            result->size = result->uncompressedSize;
+            result->uncompressedSize = 0;
+            free( output );
+        } 
+        // Otherwise, we can safely replace the original data
+        // buffer with the compressed one.
+        else {
+            free( result->valueBuffer );
+            result->valueBuffer = output;
+        }
+
         Bank * bank = _environment->expansionBanks;
 
         while( bank ) {
@@ -231,8 +261,18 @@ Variable * sequence_load( Environment * _environment, char * _filename, char * _
 
         bank->address += final->size;
         bank->remains -= final->size;
-        if ( _environment->maxExpansionBankSize[_bank_expansion] < final->size ) {
-            _environment->maxExpansionBankSize[_bank_expansion] = final->size;
+
+        // Now we must calculate the effective size occupied by
+        // memory block, when it will be uncompressed. It is needed
+        // to have enough memory into the resident part of the
+        // memory. If uncompressed size is zero, it means that
+        // the memory block is not compressed -- so we can use the
+        // size as well.
+        int realSize = result->uncompressedSize;
+        if ( realSize == 0 ) realSize = result->size;
+
+        if ( _environment->maxExpansionBankSize[_bank_expansion] < realSize ) {
+            _environment->maxExpansionBankSize[_bank_expansion] = realSize;
         }
 
     }
