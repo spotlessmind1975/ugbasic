78463dc69eb53b738dbb2e8e41f98c5c0d46c591
spotlessmind1975 <asimov@mclink.it>
2022-09-25 18:03:03 +0000

    Fixed MMOVE implementation on tms9918.

--- ugbc/src/hw/tms9918.c
+++ ugbc/src/hw/tms9918.c
@@ -2182,7 +2182,7 @@ void tms9918_move_memory_video( Environment * _environment, char * _from, char *
     outline1("LD HL, (%s)", _from );
     outline1("LD DE, (%s)", _to );
     outline1("LD BC, (%s)", _size );
-    outline0("JP VDPWRITE" );
+    outline0("CALL VDPWRITE" );
 
 }
 
@@ -2191,7 +2191,7 @@ void tms9918_move_video_memory( Environment * _environment, char * _from, char *
     outline1("LD HL, (%s)", _to );
     outline1("LD DE, (%s)", _from );
     outline1("LD BC, (%s)", _size );
-    outline0("JP VDPREAD" );
+    outline0("CALL VDPREAD" );
 
 }
 
