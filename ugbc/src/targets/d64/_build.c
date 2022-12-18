3d4230ef5db39fa3c996e9fc16c9a09e756cc4c6
spotlessmind1975 <asimov@mclink.it>
2022-12-18 19:03:02 +0000

    Refactorized building for d64.

--- ugbc/src/targets/d64/_build.c
+++ ugbc/src/targets/d64/_build.c
@@ -38,12 +38,64 @@
  * CODE SECTION 
  ****************************************************************************/
 
-void target_finalization( Environment * _environment ) {
+extern char OUTPUT_FILE_TYPE_AS_STRING[][16];
 
-    c6847_finalization( _environment );
+void target_linkage( Environment * _environment ) {
+
+    char commandLine[8*MAX_TEMPORARY_STORAGE];
+    char executableName[MAX_TEMPORARY_STORAGE];
+    char listingFileName[MAX_TEMPORARY_STORAGE];
+
+    BUILD_CHECK_FILETYPE(_environment, OUTPUT_FILE_TYPE_BIN)
+
+    BUILD_SAFE_REMOVE( _environment, _environment->exeFileName );
+
+    BUILD_TOOLCHAIN_ASM6809_GET_EXECUTABLE( _environment, executableName );
+
+    BUILD_TOOLCHAIN_ASM6809_GET_LISTING_FILE( _environment, listingFileName );
+
+    BUILD_TOOLCHAIN_ASM6809EXEC( _environment, 10240, executableName, listingFileName );
+
+    if ( _environment->listingFileName ) {
+
+        if ( _environment->profileFileName ) {
+            if ( _environment->executerFileName ) {
+                sprintf(executableName, "%s", _environment->executerFileName );
+            } else if( access( "run6809.exe", F_OK ) == 0 ) {
+                sprintf(executableName, "%s", "run6809.exe" );
+            } else {
+                sprintf(executableName, "%s", "run6809" );
+            }
+
+            sprintf( commandLine, "\"%s\" -i \"%s\" -R 2800 -b -l 27f7 \"%s\" -p \"%s\" %d",
+                executableName,
+                _environment->listingFileName,
+                _environment->exeFileName,
+                _environment->profileFileName,
+                _environment->profileCycles ? _environment->profileCycles : 1000000
+                );
+
+            if ( system_call( _environment,  commandLine ) ) {
+                printf("The profiling of assembly program failed.\n\n");
+                return;
+            }; 
+
+        }
+    
+    }
 
 }
 
-void target_analysis( Environment * _environment ) {
-    
+void target_finalize( Environment * _environment ) {
+
+}
+
+void target_cleanup( Environment * _environment ) {
+
+    remove( _environment->asmFileName );
+
+    if ( _environment->analysis && _environment->listingFileName ) {
+        target_analysis( _environment );
+    }
+
 }
