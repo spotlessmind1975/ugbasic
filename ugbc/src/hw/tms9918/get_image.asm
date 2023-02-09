dd788f213ff17938c34e0acdd2d7490ff6570542
spotlessmind1975 <asimov@mclink.it>
2023-02-09 19:57:05 +0000

    Fixed get image under tms9918.

--- ugbc/src/hw/tms9918/get_image.asm
+++ ugbc/src/hw/tms9918/get_image.asm
@@ -90,9 +90,14 @@ GETIMAGE3:
 
 GETIMAGE2:
     LD A, (HL)
-    SRL A
-    SRL A
-    SRL A
+    INC HL
+    LD B, (HL)
+    SRL B
+    RR A
+    SRL B
+    RR A
+    SRL B
+    RR A
     LD C, A
     INC HL
     LD A, (HL)
