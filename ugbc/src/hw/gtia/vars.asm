3086b74eaf3d2578d3ec68ebb468c1973a6ac062
spotlessmind1975 <asimov@mclink.it>
2023-04-10 08:12:29 +0000

    Added support for palette preserve on atari/atarixl (gtia).

--- ugbc/src/hw/gtia/vars.asm
+++ ugbc/src/hw/gtia/vars.asm
@@ -78,3 +78,223 @@ XSCROLLOFFSET:  .byte 0, 1, 2, 3, 4, 5, 6, 7
 PLOTDEST = $8a ; $8b
 PLOTCDEST= $8c ; $8d
 PLOTLDEST= $8e ; $8f
+
+@IF vestigialConfig.palettePreserve == 1
+
+; bit   index      address
+; 0     1          $02c4
+; 1     2          $02c5
+; 2     4          $02c6
+; 3     8          $02c7
+; 4     16         $02c8
+PALETTEPRESERVEUSED:
+    .byte $00
+
+@MACRO PALETTEPRESERVE2C4 value, increment
+@@
+@@  LDA PALETTEPRESERVEUSED
+@@  AND $1
+@@  CMP $1
+@@  BEQ PALETTEPRESERVE1_label
+@@
+@@  LDA value
+@@  STA $02C4
+@@
+@@  LDA PALETTEPRESERVEUSED
+@@  ORA $1
+@@  STA PALETTEPRESERVEUSED
+@@
+@@PALETTEPRESERVE1_label:
+@@
+@@  LDA increment
+@@  CMP $1
+@@  BNE PALETTEPRESERVE1_label2
+@@
+@@  INC LASTCOLOR 
+@@
+@@PALETTEPRESERVE1_label2:
+@@
+@ENDMACRO
+
+@MACRO PALETTEPRESERVE2C5 value, increment
+@@
+@@  LDA PALETTEPRESERVEUSED
+@@  AND $2
+@@  CMP $2
+@@  BEQ PALETTEPRESERVE2_label
+@@
+@@  LDA value
+@@  STA $02C5
+@@
+@@  LDA PALETTEPRESERVEUSED
+@@  ORA $2
+@@  STA PALETTEPRESERVEUSED
+@@
+@@PALETTEPRESERVE2_label:
+@@
+@@  LDA increment
+@@  CMP $1
+@@  BNE PALETTEPRESERVE2_label2
+@@
+@@  INC LASTCOLOR 
+@@
+@@PALETTEPRESERVE2_label2:
+@@
+@ENDMACRO
+
+@MACRO PALETTEPRESERVE2C6 value, increment
+@@
+@@  LDA PALETTEPRESERVEUSED
+@@  AND $4
+@@  CMP $4
+@@  BEQ PALETTEPRESERVE4_label
+@@
+@@  LDA value
+@@  STA $02C6
+@@
+@@  LDA PALETTEPRESERVEUSED
+@@  ORA $4
+@@  STA PALETTEPRESERVEUSED
+@@
+@@PALETTEPRESERVE4_label:
+@@
+@@  LDA increment
+@@  CMP $1
+@@  BNE PALETTEPRESERVE4_label2
+@@
+@@  INC LASTCOLOR 
+@@
+@@PALETTEPRESERVE4_label2:
+@@
+@ENDMACRO
+
+@MACRO PALETTEPRESERVE2C7 value, increment
+@@
+@@  LDA PALETTEPRESERVEUSED
+@@  AND $8
+@@  CMP $8
+@@  BEQ PALETTEPRESERVE8_label
+@@
+@@  LDA value
+@@  STA $02C7
+@@
+@@  LDA PALETTEPRESERVEUSED
+@@  ORA $8
+@@  STA PALETTEPRESERVEUSED
+@@
+@@PALETTEPRESERVE8_label:
+@@
+@@  LDA increment
+@@  CMP $1
+@@  BNE PALETTEPRESERVE8_label2
+@@
+@@  INC LASTCOLOR 
+@@
+@@PALETTEPRESERVE8_label2:
+@@
+@ENDMACRO
+
+@MACRO PALETTEPRESERVE2C8 value, increment
+@@
+@@  LDA PALETTEPRESERVEUSED
+@@  AND $10
+@@  CMP $10
+@@  BEQ PALETTEPRESERVE16_label
+@@
+@@  LDA value
+@@  STA $02C8
+@@
+@@  LDA PALETTEPRESERVEUSED
+@@  ORA $10
+@@  STA PALETTEPRESERVEUSED
+@@
+@@PALETTEPRESERVE16_label:
+@@
+@@  LDA increment
+@@  CMP $1
+@@  BNE PALETTEPRESERVE16_label2
+@@
+@@  INC LASTCOLOR 
+@@
+@@PALETTEPRESERVE16_label2:
+@@
+@ENDMACRO
+
+@ELSE
+
+@MACRO PALETTEPRESERVE2C4 value, increment
+@@
+@@  LDA value
+@@  STA $02C4
+@@
+@@  LDA increment
+@@  CMP $1
+@@  BNE PALETTEPRESERVE1_label2
+@@
+@@  INC LASTCOLOR 
+@@
+@@PALETTEPRESERVE1_label2:
+@@
+@ENDMACRO
+
+@MACRO PALETTEPRESERVE2C5 value, increment
+@@
+@@  LDA value
+@@  STA $02C5
+@@
+@@  LDA increment
+@@  CMP $1
+@@  BNE PALETTEPRESERVE2_label2
+@@
+@@  INC LASTCOLOR 
+@@
+@@PALETTEPRESERVE2_label2:
+@@
+@ENDMACRO
+
+@MACRO PALETTEPRESERVE2C6 value, increment
+@@
+@@  LDA value
+@@  STA $02C6
+@@
+@@  LDA increment
+@@  CMP $1
+@@  BNE PALETTEPRESERVE4_label2
+@@
+@@  INC LASTCOLOR 
+@@
+@@PALETTEPRESERVE4_label2:
+@@
+@ENDMACRO
+
+@MACRO PALETTEPRESERVE2C7 value, increment
+@@
+@@  LDA value
+@@  STA $02C7
+@@
+@@  LDA increment
+@@  CMP $1
+@@  BNE PALETTEPRESERVE8_label2
+@@
+@@  INC LASTCOLOR 
+@@
+@@PALETTEPRESERVE8_label2:
+@@
+@ENDMACRO
+
+@MACRO PALETTEPRESERVE2C8 value, increment
+@@
+@@  LDA value
+@@  STA $02C8
+@@
+@@  LDA increment
+@@  CMP $1
+@@  BNE PALETTEPRESERVE16_label2
+@@
+@@  INC LASTCOLOR 
+@@
+@@PALETTEPRESERVE16_label2:
+@@
+@ENDMACRO
+
+@ENDIF
