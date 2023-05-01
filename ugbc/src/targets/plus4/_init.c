d058b88ead3e78dc7ccea5a978d944659e163f59
spotlessmind1975 <asimov@mclink.it>
2023-05-01 20:01:59 +0000

    Moved bitmap to  (ted, plus4).

--- ugbc/src/targets/plus4/_init.c
+++ ugbc/src/targets/plus4/_init.c
@@ -67,11 +67,11 @@ void target_initialization( Environment * _environment ) {
     variable_import( _environment, "EVERYSTATUS", VT_BYTE, 0 );
     variable_global( _environment, "EVERYSTATUS" );
 
-    variable_import( _environment, "BITMAPADDRESS", VT_ADDRESS, 0x6000 );
+    variable_import( _environment, "BITMAPADDRESS", VT_ADDRESS, 0xE000 );
     variable_global( _environment, "BITMAPADDRESS" );
-    variable_import( _environment, "COLORMAPADDRESS", VT_ADDRESS, 0x5800 );
+    variable_import( _environment, "COLORMAPADDRESS", VT_ADDRESS, 0xE800 );
     variable_global( _environment, "COLORMAPADDRESS" );
-    variable_import( _environment, "TEXTADDRESS", VT_ADDRESS, 0x5c00 );
+    variable_import( _environment, "TEXTADDRESS", VT_ADDRESS, 0xEC00 );
     variable_global( _environment, "TEXTADDRESS" );    
     variable_import( _environment, "EMPTYTILE", VT_TILE, 32 );
     variable_global( _environment, "EMPTYTILE" );    
