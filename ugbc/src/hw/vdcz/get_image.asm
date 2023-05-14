b22c01bf551e5fd5bc062f3c1503bdc437dd5d16
spotlessmind1975 <asimov@mclink.it>
2023-05-14 20:08:45 +0000

    Feature Request: C128 (z80 mode without CP/M) #596

--- ugbc/src/hw/vdcz/get_image.asm
+++ ugbc/src/hw/vdcz/get_image.asm
@@ -29,12 +29,130 @@
 ;  ****************************************************************************/
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 ;*                                                                             *
-;*                          IMAGES ROUTINE FOR VDC                             *
+;*                          IMAGES ROUTINE FOR VDCZ                            *
 ;*                                                                             *
 ;*                             by Marco Spedaletti                             *
 ;*                                                                             *
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 GETIMAGE:
+
+    LD A, (CURRENTTILEMODE)
+    CP 1
+    RET Z
+
+    LD C, (HL)
+    INC HL
+    LD A, (HL)
+    LD IXH, A
+    INC HL
+    LD B, (HL)
+    INC HL
+
+    DEC IY
+    PUSH IY
+    PUSH DE
+
+    LD A, (CURRENTMODE)
+    CP 1
+    JP Z, GETIMAGE1
     RET
 
+GETIMAGE1:
+
+    PUSH BC
+    LD A, IXH
+    LD B, A
+    SRL B
+    RR C
+    SRL B
+    RR C
+    SRL B
+    RR C
+    LD A, C
+    POP BC
+    LD C, A
+    
+GETIMAGE1L2:
+
+    POP DE
+    POP IY
+
+    INC IY
+
+    PUSH IY
+    PUSH DE
+
+    PUSH HL
+    PUSH BC
+    CALL VCDZPOS
+    LD DE, HL
+    POP BC
+    POP HL
+
+    PUSH BC
+GETIMAGE1L1:
+    ; Draw them
+    CALL VDCZGETCHAR
+    ; Calculate new pixels
+    LD (HL), A
+
+    INC DE
+    INC HL
+
+    DEC C
+    JP NZ, GETIMAGE1L1
+    LD A, IXH
+    CP $0
+    JR Z, GETIMAGE1DONEROW
+
+GETIMAGE1DONEROW:
+    POP BC
+
+    LD A, (IMAGEF)
+    AND $41
+    CP $41
+    JP Z, GETIMAGE1DONEROW2
+    AND $40
+    CP $40
+    JR NZ, GETIMAGE1DONEROW2
+    OR $01
+    LD (IMAGEF), A
+
+    PUSH DE
+    LD A, C
+    LD E, A
+    LD A, 0
+    LD D, A
+    AND A
+    SBC HL, DE
+    POP DE
+    JP GETIMAGE1L2
+
+GETIMAGE1DONEROW2:
+    LD A, (IMAGEF)
+    AND $FE
+    LD (IMAGEF), A
+
+    DEC B
+    JP NZ, GETIMAGE1L2
+    
+    LD A, (IMAGET)
+    AND $2
+    CMP $2
+    JR NZ, GETIMAGEC1DONE
+
+    LD A, 26
+    LD IXH, A
+    CALL VDCZREAD
+    LD (HL), A
+
+GETIMAGEC1DONE:
+
+    POP DE
+    POP IY
+
+    JP GETIMAGEDONE
+
+GETIMAGEDONE:
+    RET
