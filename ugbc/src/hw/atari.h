782555b6d77736c87053ba60705f0cb2a42a2c04
spotlessmind1975 <asimov@mclink.it>
2023-07-17 20:50:19 +0000

    Added rate and delay support for atari/atarixl.

--- ugbc/src/hw/atari.h
+++ ugbc/src/hw/atari.h
@@ -131,6 +131,8 @@
 #define INPUT_DEFAULT_SEPARATOR     ','
 #define INPUT_DEFAULT_SIZE          32
 #define INPUT_DEFAULT_CURSOR        0xd7
+#define INPUT_DEFAULT_RATE          16
+#define INPUT_DEFAULT_DELAY         64
 
 #define SCREEN_CAPABILITIES         ( ( 1<<TILEMAP_NATIVE ) | ( 1<<BITMAP_NATIVE ) )
 
