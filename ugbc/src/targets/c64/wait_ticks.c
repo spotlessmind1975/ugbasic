874078e3d78385464f4cb1c925be4863c7901875
spotlessmind1975 <asimov@mclink.it>
2023-08-04 13:31:43 +0000

    WAIT TICKS hangs on cpc #634

--- ugbc/src/targets/c64/wait_ticks.c
+++ ugbc/src/targets/c64/wait_ticks.c
@@ -78,7 +78,7 @@ void wait_ticks_var( Environment * _environment, char * _timing ) {
 
     MAKE_LABEL
 
-    Variable * timing = variable_retrieve( _environment, _timing );
+    Variable * timing = variable_retrieve_or_define( _environment, _timing, VT_WORD, 0 );
     
     vic2_busy_wait( _environment, timing->realName );
 
