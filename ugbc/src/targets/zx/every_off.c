c16d51559c32806f4c5776ea0b1b2c3469c48c6b
spotlessmind1975 <asimov@mclink.it>
2023-08-06 19:38:32 +0000

    Fixed SCANCODE and EVERY CALL / GOSUB on zx.

--- ugbc/src/targets/zx/every_off.c
+++ ugbc/src/targets/zx/every_off.c
@@ -44,5 +44,11 @@
  * @param _environment Current calling environment
  */
 void every_off( Environment * _environment ) {
-   
+
+    if ( ! _environment->everyStatus ) {
+        CRITICAL_EVERY_OFF_WITHOUT_EVERY();
+    }
+
+    variable_store( _environment, _environment->everyStatus->name, 0x0 );
+
 }
