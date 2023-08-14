c186306af92ee03d066d53df72e5c969a11f46e4
spotlessmind1975 <asimov@mclink.it>
2023-08-14 17:13:06 +0000

    Fixed black color for tms9918 (coleco).

--- ugbc/src/targets/coleco/csprite_init.c
+++ ugbc/src/targets/coleco/csprite_init.c
@@ -75,7 +75,7 @@ Variable * csprite_init( Environment * _environment, char * _image, char *_sprit
     int i = 0, c = 0;
 
     for (i=0; i<image->originalColors; ++i ) {
-        if ( image->originalPalette[i].index == COLOR_BLACK || image->originalPalette[i].index == COLOR_TRANSPARENT ) continue;
+        if ( image->originalPalette[i].index == COLOR_TRANSPARENT ) continue;
         ++c;
         variable_move_naked( _environment, spriteCount->name, index->name );
         Variable * realImage = sprite_converter( _environment, image->originalBitmap, image->originalWidth, image->originalHeight, image->originalDepth, &image->originalPalette[i], _flags );
