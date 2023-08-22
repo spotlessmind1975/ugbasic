e51b83f044d821ff0c75a5890884d60000a49c74
spotlessmind1975 <asimov@mclink.it>
2023-08-22 06:10:11 +0000

    Added script for massive build of compilers.

---
+++ build_all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+build () {
+
+    echo -n $1
+    echo -n ": "
+
+    make target=$1 clean compiler >/dev/null 2>/dev/null
+
+    status=$?
+    [ $status -eq 0 ] && echo "built" || echo "ERROR"
+
+}
+
+build atari 
+build atarixl
+build c128
+build c64
+build coleco
+build cpc
+build d32
+build d64
+build mo5
+build msx1
+build pc128op
+build plus4
+build sc3000
+build sg1000
+build vg5000
+build vic20
+build zx
+build coco
+build c128z
