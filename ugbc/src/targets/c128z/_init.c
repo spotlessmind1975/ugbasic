e6b1eec068f3293cd567f3bb154bd78ff9e3ced1
spotlessmind1975 <asimov@mclink.it>
2023-08-29 20:28:07 +0000

    Added missing TICKSPERSECOND variable.

--- ugbc/src/targets/c128z/_init.c
+++ ugbc/src/targets/c128z/_init.c
@@ -103,6 +103,9 @@ void target_initialization( Environment * _environment ) {
     variable_import( _environment, "IRQVECTORREADY", VT_BYTE, 0 );
     variable_global( _environment, "IRQVECTORREADY" );   
 
+    variable_import( _environment, "TICKSPERSECOND", VT_BYTE, 0 );
+    variable_global( _environment, "TICKSPERSECOND" );   
+
     variable_import( _environment, "FPSCRAP", VT_BUFFER, 16 );
     variable_global( _environment, "FPSCRAP" );
 
