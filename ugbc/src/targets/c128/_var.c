5eca11813171d9e4338d8a24607580b0927213c9
spotlessmind1975 <asimov@mclink.it>
2023-09-08 07:35:23 +0000

    Added preliminary support for strings dictionary on c128.

--- ugbc/src/targets/c128/_var.c
+++ ugbc/src/targets/c128/_var.c
@@ -94,17 +94,18 @@ static void variable_cleanup_entry( Environment * _environment, Variable * _firs
                     if ( variable->memoryArea && !variable->bankAssigned ) {
                         // outline2("%s = $%4.4x", variable->realName, variable->absoluteAddress);
                     } else {
-                        if ( variable->printable ) {
-                            int c = strlen( variable->valueString->value );
-                            out2("%s: .byte %d,", variable->realName, c);
-                            int i=0;
-                            for (i=0; i<(c-1); ++i ) {
-                                out1("$%2.2x,", (unsigned char)variable->valueString->value[i]);
-                            }
-                            outline1("$%2.2x", (unsigned char)variable->valueString->value[(c-1)]);                        
-                        } else {
-                            outline3("%s: .byte %d,%s", variable->realName, (int)strlen(variable->valueString->value), escape_newlines( variable->valueString->value ) );
-                        }
+                        // if ( variable->printable ) {
+                        //     int c = strlen( variable->valueString->value );
+                        //     out2("%s: .byte %d,", variable->realName, c);
+                        //     int i=0;
+                        //     for (i=0; i<(c-1); ++i ) {
+                        //         out1("$%2.2x,", (unsigned char)variable->valueString->value[i]);
+                        //     }
+                        //     outline1("$%2.2x", (unsigned char)variable->valueString->value[(c-1)]);                        
+                        // } else {
+                        //     outline3("%s: .byte %d,%s", variable->realName, (int)strlen(variable->valueString->value), escape_newlines( variable->valueString->value ) );
+                        // }
+                        outline2("%s = cstring%d", variable->realName, variable->valueString->id );
                     }
                     break;
                 case VT_DSTRING:
@@ -247,17 +248,18 @@ static void variable_cleanup_memory_mapped( Environment * _environment, Variable
             break;
         }
         case VT_STRING:
-            if ( _variable->printable ) {
-                int c = strlen( _variable->valueString->value );
-                out1("   .byte %d,", c);
-                int i=0;
-                for (i=0; i<(c-1); ++i ) {
-                    out1("$%2.2x,", (unsigned char)_variable->valueString->value[i]);
-                }
-                outline1("$%2.2x", (unsigned char)_variable->valueString->value[(c-1)]);                        
-            } else {
-                outline2("   .byte %d,%s", (int)strlen(_variable->valueString->value), escape_newlines( _variable->valueString->value ) );
-            }
+            // if ( _variable->printable ) {
+            //     int c = strlen( _variable->valueString->value );
+            //     out1("   .byte %d,", c);
+            //     int i=0;
+            //     for (i=0; i<(c-1); ++i ) {
+            //         out1("$%2.2x,", (unsigned char)_variable->valueString->value[i]);
+            //     }
+            //     outline1("$%2.2x", (unsigned char)_variable->valueString->value[(c-1)]);                        
+            // } else {
+            //     outline2("   .byte %d,%s", (int)strlen(_variable->valueString->value), escape_newlines( _variable->valueString->value ) );
+            // }
+            outline2("%s = cstring%d", _variable->realName, _variable->valueString->id );
             break;
         case VT_DSTRING:
         case VT_SPRITE:
@@ -459,5 +461,10 @@ void variable_cleanup( Environment * _environment ) {
 
     variable_on_memory_init( _environment, 0 );
 
+    StaticString * staticStrings = _environment->strings;
+    while( staticStrings ) {
+        outline3("cstring%d: .byte %d, %s", staticStrings->id, (int)strlen(staticStrings->value), escape_newlines( staticStrings->value ) );
+        staticStrings = staticStrings->next;
+    }
 
 }
