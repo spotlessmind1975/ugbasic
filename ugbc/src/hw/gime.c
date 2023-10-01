9bab9cec67076c4b8a46271f83035340791360ab
spotlessmind1975 <asimov@mclink.it>
2023-10-01 16:48:15 +0000

    Add support for TRS-80 Color Computer version 3 #586 - moved GIME segs

--- ugbc/src/hw/gime.c
+++ ugbc/src/hw/gime.c
@@ -1103,21 +1103,20 @@ int gime_screen_mode_enable( Environment * _environment, ScreenMode * _screen_mo
 
     if ( currentFrameSize <= 0x2000 ) {
         cpu_store_8bit( _environment, "GIMEMMUSTART", 3 );
-        cpu_store_16bit( _environment, "BITMAPADDRESS", 0xe000 );
-        cpu_store_16bit( _environment, "TEXTADDRESS", 0xe000 );
-    } else 
-    if ( currentFrameSize <= 0x4000 ) {
-        cpu_store_8bit( _environment, "GIMEMMUSTART", 2 );
         cpu_store_16bit( _environment, "BITMAPADDRESS", 0xc000 );
         cpu_store_16bit( _environment, "TEXTADDRESS", 0xc000 );
-    } else if ( currentFrameSize <= 0x6000 ) {
-        cpu_store_8bit( _environment, "GIMEMMUSTART", 1 );
+    } else if ( currentFrameSize <= 0x4000 ) {
+        cpu_store_8bit( _environment, "GIMEMMUSTART", 2 );
         cpu_store_16bit( _environment, "BITMAPADDRESS", 0xa000 );
         cpu_store_16bit( _environment, "TEXTADDRESS", 0xa000 );
-    } else {
-        cpu_store_8bit( _environment, "GIMEMMUSTART", 0 );
+    } else if ( currentFrameSize <= 0x6000 ) {
+        cpu_store_8bit( _environment, "GIMEMMUSTART", 1 );
         cpu_store_16bit( _environment, "BITMAPADDRESS", 0x8000 );
         cpu_store_16bit( _environment, "TEXTADDRESS", 0x8000 );
+    } else {
+        cpu_store_8bit( _environment, "GIMEMMUSTART", 0 );
+        cpu_store_16bit( _environment, "BITMAPADDRESS", 0x4000 );
+        cpu_store_16bit( _environment, "TEXTADDRESS", 0x4000 );
     }
 
     cpu_store_16bit( _environment, "ORIGINX", 0 );
