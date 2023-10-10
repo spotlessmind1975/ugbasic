5402bf5768b8c97cb8e07bd4b121dc28ea1fa8d0
spotlessmind1975 <asimov@mclink.it>
2023-10-10 13:49:39 +0000

    Added embedded version for 8 bit mul/div power of 2 on z80.

---
+++ ugbc/src/hw/z80/cpu_math_mul2_const_8bit.asm
@@ -0,0 +1,72 @@
+; /*****************************************************************************
+;  * ugBASIC - an isomorphic BASIC language compiler for retrocomputers        *
+;  *****************************************************************************
+;  * Copyright 2021-2023 Marco Spedaletti (asimov@mclink.it)
+;  *
+;  * Licensed under the Apache License, Version 2.0 (the "License");
+;  * you may not use this file except in compliance with the License.
+;  * You may obtain a copy of the License at
+;  *
+;  * http://www.apache.org/licenses/LICENSE-2.0
+;  *
+;  * Unless required by applicable law or agreed to in writing, software
+;  * distributed under the License is distributed on an "AS IS" BASIS,
+;  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;  * See the License for the specific language governing permissions and
+;  * limitations under the License.
+;  *----------------------------------------------------------------------------
+;  * Concesso in licenza secondo i termini della Licenza Apache, versione 2.0
+;  * (la "Licenza"); è proibito usare questo file se non in conformità alla
+;  * Licenza. Una copia della Licenza è disponibile all'indirizzo:
+;  *
+;  * http://www.apache.org/licenses/LICENSE-2.0
+;  *
+;  * Se non richiesto dalla legislazione vigente o concordato per iscritto,
+;  * il software distribuito nei termini della Licenza è distribuito
+;  * "COSì COM'è", SENZA GARANZIE O CONDIZIONI DI ALCUN TIPO, esplicite o
+;  * implicite. Consultare la Licenza per il testo specifico che regola le
+;  * autorizzazioni e le limitazioni previste dalla medesima.
+;  ****************************************************************************/
+;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;*                                                                             *
+;*                           MUL2 CONST 8 BIT ON Z80                           *
+;*                                                                             *
+;*                             by Marco Spedaletti                             *
+;*                                                                             *
+;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+; B * 2^C -> B (signed)
+CPUMUL2CONST8S:
+    LD A, B
+    AND $80
+    PUSH AF
+    JR Z, CPUMUL2CONST8SPOS
+
+    LD A, B
+    XOR $FF
+    LD B, A
+
+    INC B
+
+CPUMUL2CONST8SPOS:
+    SLA B
+    DEC C
+    JR NZ, CPUMUL2CONST8SPOS
+
+    POP AF
+    AND $80
+    RET Z
+
+    LD A, B
+    XOR $FF
+    LD B, A
+
+    INC B
+    RET
+
+CPUMUL2CONST8U:
+CPUMUL2CONST8UL1:
+    SLA B
+    DEC C
+    JR NZ, CPUMUL2CONST8UL1
+    RET
