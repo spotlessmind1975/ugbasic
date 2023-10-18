a80e8251daa17fa521e5780b23873fb2ecc8a7e8
spotlessmind1975 <asimov@mclink.it>
2023-10-18 20:59:32 +0000

    Add READ / DATA instructions #296 - c128

--- ugbc/src/targets/c128z/_init.c
+++ ugbc/src/targets/c128z/_init.c
@@ -126,6 +126,9 @@ void target_initialization( Environment * _environment ) {
     variable_import( _environment, "KBDDELAYC", VT_BYTE, 64 );
     variable_global( _environment, "KBDDELAYC" );
 
+    variable_import( _environment, "DATAPTR", VT_ADDRESS, 0 );
+    variable_global( _environment, "DATAPTR" );
+
     bank_define( _environment, "VARIABLES", BT_VARIABLES, 0x5000, NULL );
     bank_define( _environment, "TEMPORARY", BT_TEMPORARY, 0x5100, NULL );
 
