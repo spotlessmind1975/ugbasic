3bab8c499f8acfd77ebce6be00d12ac4f3d507d3
spotlessmind1975 <asimov@mclink.it>
2023-10-18 21:47:11 +0000

    Add READ / DATA instructions #296 - coleco

--- ugbc/src/targets/coleco/_init.c
+++ ugbc/src/targets/coleco/_init.c
@@ -128,7 +128,10 @@ void target_initialization( Environment * _environment ) {
 
     variable_import( _environment, "FPSCRAP", VT_BUFFER, 16 );
     variable_global( _environment, "FPSCRAP" );
-    
+
+    variable_import( _environment, "DATAPTR", VT_ADDRESS, 0 );
+    variable_global( _environment, "DATAPTR" );
+
     bank_define( _environment, "VARIABLES", BT_VARIABLES, 0x5000, NULL );
     bank_define( _environment, "TEMPORARY", BT_TEMPORARY, 0x5100, NULL );
 
