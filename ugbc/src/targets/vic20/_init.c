a56686742dd2642cf898835bc71531fb92a53647
spotlessmind1975 <asimov@mclink.it>
2023-10-28 20:43:42 +0000

    Add READ / DATA instructions #296 - vic20

--- ugbc/src/targets/vic20/_init.c
+++ ugbc/src/targets/vic20/_init.c
@@ -111,7 +111,7 @@ void target_initialization( Environment * _environment ) {
 
     vic1_initialization( _environment );
 
-    deploy( startup, src_hw_vic20_startup_asm);
+    deploy_deferred( startup, src_hw_vic20_startup_asm);
     cpu_call( _environment, "VIC20STARTUP" );
 
     if ( _environment->tenLinerRulesEnforced ) {
