a6411b2a160dfe0c99b1b38020dfe6ce8cbd1407
spotlessmind1975 <asimov@mclink.it>
2023-11-17 07:37:36 +0000

    BIT ... OF returns 255 instead of TRUE #706

--- ugbc/src/targets/common/_infrastructure.c
+++ ugbc/src/targets/common/_infrastructure.c
@@ -8150,7 +8150,7 @@ Altrimenti, l'istruzione restituirà il valore `TRUE`.
 Variable * variable_bit( Environment * _environment, char * _value, char * _position ) {
     Variable * value = variable_retrieve_or_define( _environment, _value, VT_DWORD, 0 );
     Variable * position = variable_retrieve_or_define( _environment, _position, VT_BYTE, 1 );
-    Variable * result = variable_temporary( _environment, VT_BYTE, "(result of BIT)");
+    Variable * result = variable_temporary( _environment, VT_SBYTE, "(result of BIT)");
 
     MAKE_LABEL
 
