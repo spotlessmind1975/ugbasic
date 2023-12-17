d12c3d38adaf90410d3ba51f30372058da536e7e
spotlessmind1975 <asimov@mclink.it>
2023-12-17 16:00:11 +0000

    Added support for joystick reading under irq (c128).

---
+++ ugbc/src/hw/c128/joystick.asm
@@ -0,0 +1,70 @@
+; /*****************************************************************************
+;  * ugBASIC - an isomorphic BASIC language compiler for retrocomputers        *
+;  *****************************************************************************
+;  * Copyright 2021-2023 Marco Spedaletti (asimov@mclink.it)
+;  *
+;  * Licensed under the Apache License, Version 2.0 (the "License");
+;  * you may not use this file except in compliance with the License.
+;  * You may obtain a copy of the License at
+;  *
+;  * http://www.apache.org/licenses/LICENSE-2.0
+;  *
+;  * Unless required by applicable law or agreed to in writing, software
+;  * distributed under the License is distributed on an "AS IS" BASIS,
+;  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;  * See the License for the specific language governing permissions and
+;  * limitations under the License.
+;  *----------------------------------------------------------------------------
+;  * Concesso in licenza secondo i termini della Licenza Apache, versione 2.0
+;  * (la "Licenza"); è proibito usare questo file se non in conformità alla
+;  * Licenza. Una copia della Licenza è disponibile all'indirizzo:
+;  *
+;  * http://www.apache.org/licenses/LICENSE-2.0
+;  *
+;  * Se non richiesto dalla legislazione vigente o concordato per iscritto,
+;  * il software distribuito nei termini della Licenza è distribuito
+;  * "COSì COM'è", SENZA GARANZIE O CONDIZIONI DI ALCUN TIPO, esplicite o
+;  * implicite. Consultare la Licenza per il testo specifico che regola le
+;  * autorizzazioni e le limitazioni previste dalla medesima.
+;  ****************************************************************************/
+;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;*                                                                             *
+;*                          JOYSTICK DETECTION ON C=128                        *
+;*                                                                             *
+;*                             by Marco Spedaletti                             *
+;*                                                                             *
+;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+JOYSTICKMANAGER:
+    
+    PHP
+    PHA
+    TXA
+    PHA
+    TYA
+    PHA
+
+    LDA #$7F
+    STA $DC00
+    LDA $DC01
+    AND #$1F
+    EOR #$1F
+    STA JOYSTICK0
+
+    LDA #$E0
+    LDY #$FF
+    STA $DC02
+    LDA $DC00
+    STY $DC02
+    AND #$1F
+    EOR #$1F
+    STA JOYSTICK1
+
+    PLA
+    TAY
+    PLA
+    TAX
+    PLA
+    PLP
+
+    RTS
