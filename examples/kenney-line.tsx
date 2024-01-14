eae91d3c51bdff9d8c884d87e31c4d733d698cbc
spotlessmind1975 <asimov@mclink.it>
2024-01-14 12:21:19 +0000

    Added examples.

---
+++ examples/kenney-line.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="kenney-line" tilewidth="16" tileheight="16" tilecount="60" columns="10">
+ <image source="kenney_line.png" width="160" height="96"/>
+</tileset>
