5a522a62cfa03fc765459589445e90bfc90ec5d2
spotlessmind1975 <asimov@mclink.it>
2024-01-20 15:23:22 +0000

    Fixed palette on EF936x (mo5, pc128op).

--- ugbc/src/hw/ef936x.h
+++ ugbc/src/hw/ef936x.h
@@ -71,12 +71,15 @@
 #elif defined(__mo5__)
 
     #define COLOR_BLACK					0
+    #define COLOR_DARK_RED				1 // RED
     #define COLOR_RED					1
+    #define COLOR_DARK_GREEN			2 // GREEN
     #define COLOR_GREEN					2
     #define COLOR_YELLOW				3
     #define COLOR_BLUE					4
     #define COLOR_VIOLET				5
     #define COLOR_CYAN					6
+    #define COLOR_DARK_WHITE			7 // WHITE
     #define COLOR_WHITE					7
     #define COLOR_GREY					8
     #define COLOR_OLD_ROSE  			9
