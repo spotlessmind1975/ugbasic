73dc11620b91ecd1633d1dbffe4ff279d0fd8bba
spotlessmind1975 <asimov@mclink.it>
2024-01-22 22:27:52 +0000

    Avoid to deploy font under ef936x if PRINT is missing (mo5, pc128op).

--- ugbc/src/hw/ef936x.c
+++ ugbc/src/hw/ef936x.c
@@ -761,7 +761,7 @@ void ef936x_initialization( Environment * _environment ) {
     cpu_store_8bit( _environment, "CURRENTTILESWIDTH", _environment->screenTilesWidth );
     cpu_store_8bit( _environment, "CURRENTTILESHEIGHT", _environment->screenTilesHeight );
 
-    font_descriptors_init( _environment, 0 );
+    font_descriptors_init( _environment, 1 );
 
     _environment->currentRgbConverterFunction = rgbConverterFunction;
     _environment->screenShades = 4096;
