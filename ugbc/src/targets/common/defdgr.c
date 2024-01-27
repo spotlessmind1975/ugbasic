3ec28095b92f92161b1776f144e2e4446d923ff9
spotlessmind1975 <asimov@mclink.it>
2024-01-27 08:56:24 +0000

    DEFDGR only on redefined fonts.

--- ugbc/src/targets/common/defdgr.c
+++ ugbc/src/targets/common/defdgr.c
@@ -70,28 +70,32 @@ si vuole personalizzare il font al momento della compilazione, far riferimento a
 </usermanual> */
 void defdgr_vars( Environment * _environment, char * _character, char * _b0, char * _b1, char * _b2, char * _b3, char * _b4, char * _b5, char * _b6, char * _b7 ) {
 
-    Variable * address = variable_temporary( _environment, VT_ADDRESS, "(address)");
+    if ( _environment->descriptors ) {
 
-    Variable * offset = variable_retrieve_or_define( _environment, _character, VT_WORD, 0 );
-    variable_mul2_const( _environment, offset->name, 3 );
-    Variable * b0 = variable_retrieve_or_define( _environment, _b0, VT_BYTE, 0 );
-    Variable * b1 = variable_retrieve_or_define( _environment, _b1, VT_BYTE, 0 );
-    Variable * b2 = variable_retrieve_or_define( _environment, _b2, VT_BYTE, 0 );
-    Variable * b3 = variable_retrieve_or_define( _environment, _b3, VT_BYTE, 0 );
-    Variable * b4 = variable_retrieve_or_define( _environment, _b4, VT_BYTE, 0 );
-    Variable * b5 = variable_retrieve_or_define( _environment, _b5, VT_BYTE, 0 );
-    Variable * b6 = variable_retrieve_or_define( _environment, _b6, VT_BYTE, 0 );
-    Variable * b7 = variable_retrieve_or_define( _environment, _b7, VT_BYTE, 0 );
+        Variable * address = variable_temporary( _environment, VT_ADDRESS, "(address)");
 
-    cpu_addressof_16bit( _environment, "UDCCHAR", address->realName );
-    cpu_math_add_16bit( _environment, address->realName, offset->realName, address->realName );
-    cpu_move_8bit_indirect_with_offset( _environment, b0->realName, address->realName, 0 );    
-    cpu_move_8bit_indirect_with_offset( _environment, b1->realName, address->realName, 1 );    
-    cpu_move_8bit_indirect_with_offset( _environment, b2->realName, address->realName, 2 );    
-    cpu_move_8bit_indirect_with_offset( _environment, b3->realName, address->realName, 3 );    
-    cpu_move_8bit_indirect_with_offset( _environment, b4->realName, address->realName, 4);    
-    cpu_move_8bit_indirect_with_offset( _environment, b5->realName, address->realName, 5 );    
-    cpu_move_8bit_indirect_with_offset( _environment, b6->realName, address->realName, 6);    
-    cpu_move_8bit_indirect_with_offset( _environment, b7->realName, address->realName, 7 );    
+        Variable * offset = variable_retrieve_or_define( _environment, _character, VT_WORD, 0 );
+        variable_mul2_const( _environment, offset->name, 3 );
+        Variable * b0 = variable_retrieve_or_define( _environment, _b0, VT_BYTE, 0 );
+        Variable * b1 = variable_retrieve_or_define( _environment, _b1, VT_BYTE, 0 );
+        Variable * b2 = variable_retrieve_or_define( _environment, _b2, VT_BYTE, 0 );
+        Variable * b3 = variable_retrieve_or_define( _environment, _b3, VT_BYTE, 0 );
+        Variable * b4 = variable_retrieve_or_define( _environment, _b4, VT_BYTE, 0 );
+        Variable * b5 = variable_retrieve_or_define( _environment, _b5, VT_BYTE, 0 );
+        Variable * b6 = variable_retrieve_or_define( _environment, _b6, VT_BYTE, 0 );
+        Variable * b7 = variable_retrieve_or_define( _environment, _b7, VT_BYTE, 0 );
+
+        cpu_addressof_16bit( _environment, "UDCCHAR", address->realName );
+        cpu_math_add_16bit( _environment, address->realName, offset->realName, address->realName );
+        cpu_move_8bit_indirect_with_offset( _environment, b0->realName, address->realName, 0 );    
+        cpu_move_8bit_indirect_with_offset( _environment, b1->realName, address->realName, 1 );    
+        cpu_move_8bit_indirect_with_offset( _environment, b2->realName, address->realName, 2 );    
+        cpu_move_8bit_indirect_with_offset( _environment, b3->realName, address->realName, 3 );    
+        cpu_move_8bit_indirect_with_offset( _environment, b4->realName, address->realName, 4);    
+        cpu_move_8bit_indirect_with_offset( _environment, b5->realName, address->realName, 5 );    
+        cpu_move_8bit_indirect_with_offset( _environment, b6->realName, address->realName, 6);    
+        cpu_move_8bit_indirect_with_offset( _environment, b7->realName, address->realName, 7 );    
+
+    }
 
 }
