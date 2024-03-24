b1c2befc90887fbd51cf183f3c0393b2d95a75a8
spotlessmind1975 <asimov@mclink.it>
2024-03-24 23:40:20 +0000

    Fixed add 32 bit under 6809.

--- ugbc/src/hw/6809.c
+++ ugbc/src/hw/6809.c
@@ -2451,11 +2451,11 @@ void cpu6809_math_add_32bit_const( Environment * _environment, char *_source, in
         outline1("LDD %s", address_displacement(_environment, _source, "2"));
         outline1("LDX #$%4.4x", ( _destination & 0xffff ) );
         outline0("LEAX D,X");
-        outline1("STD %s", address_displacement(_environment, _other, "2"));
+        outline1("STX %s", address_displacement(_environment, _other, "2"));
         outline1("LDD %s", _source);
         outline1("LDX #$%4.4x", ( ( _destination >> 16 ) & 0xffff ) );
         outline0("LEAX D,X");
-        outline1("STD %s", _other ? _other : ",X" );
+        outline1("STX %s", _other ? _other : ",X" );
 
     no_embedded( cpu_math_add_32bit_const )
 
