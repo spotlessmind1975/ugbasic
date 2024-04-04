abadee7a853f17bb717c55883fc02ddbca77eaae
spotlessmind1975 <asimov@mclink.it>
2024-04-04 20:37:37 +0000

    Refactorized bank access d64.

--- ugbc/src/targets/common/6809/bank_uncompress.c
+++ ugbc/src/targets/common/6809/bank_uncompress.c
@@ -34,7 +34,7 @@
 
 #include "../../../ugbc.h"
 
-#if defined(__coco__) || defined(__d32__)
+#if defined(__coco__) || defined(__d32__) || defined(__d64__)
 
 /**
  * @brief Emit ASM code for instruction <b>BANK UNCOMPRESS ...</b>
