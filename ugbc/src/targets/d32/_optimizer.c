ab0a851bea94a163b8dc1a5327697ffa06a710bf
spotlessmind1975 <asimov@mclink.it>
2024-04-06 11:44:27 +0000

    Fixed optimization on 6809 when A+B or D register is used.

--- ugbc/src/targets/d32/_optimizer.c
+++ ugbc/src/targets/d32/_optimizer.c
@@ -361,7 +361,8 @@ static void basic_peephole(Environment * _environment, POBuffer buf[LOOK_AHEAD],
 
     if ( (po_buf_match(buf[0], " LD* ", v1) || po_buf_match(buf[0], " CLR*", v1))
     &&   po_buf_match(buf[1], " LD* *,*", v2, v3, v4)
-    &&  po_buf_strcmp(v1,v2)==0 && po_buf_strcmp(v2,v3)!=0 ) {
+    &&  po_buf_strcmp(v1,v2)==0 && po_buf_strcmp(v2,v3)!=0
+    &&  ( strcmp(v1->str,"A")!=0 && strcmp(v1->str,"B")!=0 && strcmp(v3->str,"D")!=0 ) ) {
         optim(buf[0], RULE "(LOAD/CLR,LOAD)->(LOAD)", NULL);
         ++_environment->removedAssemblyLines;
     }
