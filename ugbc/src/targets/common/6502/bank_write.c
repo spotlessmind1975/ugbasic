b8259b18b1344e3d5757627aefcdb9c69b453de0
spotlessmind1975 <asimov@mclink.it>
2024-04-11 15:42:15 +0000

    Fixed banks support under 6502.

--- ugbc/src/targets/common/6502/bank_write.c
+++ ugbc/src/targets/common/6502/bank_write.c
@@ -66,6 +66,17 @@ void bank_write_vars( Environment * _environment, char * _address1, char * _bank
     
 }
 
+void bank_write_vars_direct( Environment * _environment, char * _address1, char * _bank, char * _address2, char * _size ) {
+
+    outline0("; bank write")
+    Variable * address1 = variable_retrieve( _environment, _address1 );
+    Variable * effectiveAddress = variable_temporary( _environment, VT_ADDRESS, "(effectiveAddress)");
+    cpu_addressof_16bit( _environment, address1->realName, effectiveAddress->realName );
+    bank_write_vars( _environment, effectiveAddress->name, _bank, _address2, _size );
+    outline0("; end bank write")
+
+}
+
 void bank_write_vars_bank_direct_size( Environment * _environment, char * _address1, int _bank, char * _address2, int _size ) {
 
     outline0("; bank write")
