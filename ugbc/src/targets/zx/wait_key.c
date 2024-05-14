b7ced4574e45f705a0cf4ee05c405553f805d065
spotlessmind1975 <asimov@mclink.it>
2024-05-14 22:13:36 +0000

    Fixed WAIT KEY under zx.

--- ugbc/src/targets/zx/wait_key.c
+++ ugbc/src/targets/zx/wait_key.c
@@ -49,11 +49,18 @@ void wait_key( Environment * _environment ) {
     Variable * pressed = variable_temporary( _environment, VT_BYTE, "(key pressed?)");
 
     char repeatLabel[MAX_TEMPORARY_STORAGE]; sprintf(repeatLabel, "%srepeat", label );
+    char repeatLabel2[MAX_TEMPORARY_STORAGE]; sprintf(repeatLabel2, "%srepeat2", label );
 
     cpu_label( _environment, repeatLabel );
 
     zx_scancode( _environment, pressed->realName, result->realName );
 
     cpu_bveq( _environment, pressed->realName, repeatLabel );
+
+    cpu_label( _environment, repeatLabel2 );
+
+    zx_scancode( _environment, pressed->realName, result->realName );
+
+    cpu_bvneq( _environment, pressed->realName, repeatLabel2 );
 
 }
