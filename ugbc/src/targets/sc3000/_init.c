94eb5591eb353febccce626fc302890667accb85
spotlessmind1975 <asimov@mclink.it>
2024-05-20 14:54:16 +0000

    Migrate inline code to library call on demand #134 #135 #136 - bit manipulation

--- ugbc/src/targets/sc3000/_init.c
+++ ugbc/src/targets/sc3000/_init.c
@@ -65,7 +65,8 @@ void setup_embedded( Environment * _environment ) {
     _environment->embedded.cpu_math_div2_const_16bit = 1;
     _environment->embedded.cpu_compare_16bit = 1;
     _environment->embedded.cpu_compare_32bit = 1;
-    
+    _environment->embedded.cpu_bit_inplace = 1;
+
 }
 
 void target_initialization( Environment * _environment ) {
