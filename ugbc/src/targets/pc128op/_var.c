f5be006d18814f8cbb759ac3d4d9b450a88328b3
spotlessmind1975 <asimov@mclink.it>
2024-06-06 06:31:28 +0000

    Added preliminary support for sprite multiplexing under ef936x (no-op).

--- ugbc/src/targets/pc128op/_var.c
+++ ugbc/src/targets/pc128op/_var.c
@@ -317,6 +317,13 @@ static void variable_cleanup_entry_byte( Environment * _environment, Variable * 
                         outhead1("%s rzb 1", variable->realName);
                     }   
                     break;
+                case VT_MSPRITE:
+                    if ( variable->memoryArea ) {
+                        outhead2("%s equ $%4.4x", variable->realName, variable->absoluteAddress);
+                    } else {
+                        outhead1("%s rzb 2", variable->realName);
+                    }   
+                    break;
                 case VT_TILE:
                     if ( variable->memoryArea ) {
                         outhead2("%s equ $%4.4x", variable->realName, variable->absoluteAddress);
