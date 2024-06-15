9c032f77941dc8e52f1d3f31b6b81fe0502cb97f
spotlessmind1975 <asimov@mclink.it>
2024-06-15 15:09:37 +0000

    Removed debugging.

--- ugbc/src/targets/common/_infrastructure.c
+++ ugbc/src/targets/common/_infrastructure.c
@@ -1236,8 +1236,6 @@ Variable * variable_store( Environment * _environment, char * _destination, unsi
     
     Variable * destination = variable_retrieve( _environment, _destination );
 
-    printf( "%s (%d), da %d a %d\n", _destination, destination->initializedByConstant, destination->value, _value );
-    
     if ( !destination->initializedByConstant ) {
         destination->value = _value;
     }
