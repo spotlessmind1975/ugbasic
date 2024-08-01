e23f00499626e44561f89a176eaffba26dd3db55
spotlessmind1975 <asimov@mclink.it>
2024-08-01 11:37:31 +0000

    Added support for print function with NULL.

--- ugbc/src/targets/common/print.c
+++ ugbc/src/targets/common/print.c
@@ -127,196 +127,200 @@ void print( Environment * _environment, char * _value, int _new_line ) {
 
     MAKE_LABEL
 
-    Variable * value = variable_retrieve_or_define( _environment, _value, VT_DSTRING, 0 );
+    if ( _value ) {
 
-    if ( value->type != VT_DSTRING && value->type != VT_STRING && value->type != VT_CHAR ) {
-        switch( VT_BITWIDTH( value->type ) ) {
-            case 32:
-            case 16:
-            case 8: {
-                Variable * address = variable_temporary( _environment, VT_ADDRESS, "(temporary for PRINT)");
-                Variable * size = variable_temporary( _environment, VT_BYTE, "(temporary for PRINT)");
-                Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
-                variable_store_string( _environment, tmp->name, "          " );
+        Variable * value = variable_retrieve_or_define( _environment, _value, VT_DSTRING, 0 );
 
-                cpu_dswrite( _environment, tmp->realName );
+        if ( value->type != VT_DSTRING && value->type != VT_STRING && value->type != VT_CHAR ) {
+            switch( VT_BITWIDTH( value->type ) ) {
+                case 32:
+                case 16:
+                case 8: {
+                    Variable * address = variable_temporary( _environment, VT_ADDRESS, "(temporary for PRINT)");
+                    Variable * size = variable_temporary( _environment, VT_BYTE, "(temporary for PRINT)");
+                    Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
+                    variable_store_string( _environment, tmp->name, "          " );
 
-                cpu_dsdescriptor( _environment, tmp->realName, address->realName, size->realName );
+                    cpu_dswrite( _environment, tmp->realName );
 
-                cpu_number_to_string( _environment, value->realName, address->realName, size->realName, VT_BITWIDTH( value->type ), VT_SIGNED( value->type ) );
+                    cpu_dsdescriptor( _environment, tmp->realName, address->realName, size->realName );
 
-                cpu_dsresize( _environment, tmp->realName, size->realName );
+                    cpu_number_to_string( _environment, value->realName, address->realName, size->realName, VT_BITWIDTH( value->type ), VT_SIGNED( value->type ) );
 
-                value = tmp;
-                
-                break;
-            }
-            case 1: {
-                Variable * address = variable_temporary( _environment, VT_ADDRESS, "(temporary for PRINT)");
-                Variable * size = variable_temporary( _environment, VT_BYTE, "(temporary for PRINT)");
-                Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
-                Variable * bcheck = variable_temporary( _environment, VT_BYTE, "(temporary for PRINT)");
-                Variable * zero = variable_temporary( _environment, VT_BYTE, "(temporary for PRINT)");
-                Variable * one = variable_temporary( _environment, VT_BYTE, "(temporary for PRINT)");
+                    cpu_dsresize( _environment, tmp->realName, size->realName );
 
-                cpu_store_8bit( _environment, zero->realName, '0' );
-                cpu_store_8bit( _environment, one->realName, '1' );
-
-                variable_store_string( _environment, tmp->name, " " );
-                cpu_dswrite( _environment, tmp->realName );
-                cpu_dsdescriptor( _environment, tmp->realName, address->realName, size->realName );
-
-                MAKE_LABEL
-
-                char doneLabel[MAX_TEMPORARY_STORAGE]; sprintf( doneLabel, "%sdone", label );
-
-                outline0(" ; printing bit..." );
-                cpu_bit_check( _environment, value->realName, value->bitPosition, bcheck->realName, 8 );
-                cpu_compare_and_branch_8bit_const( _environment, bcheck->realName, 0, label, 1 );
-                cpu_move_8bit_indirect( _environment, one->realName, address->realName );
-                cpu_jump( _environment, doneLabel );
-                cpu_label( _environment, label );
-                cpu_move_8bit_indirect( _environment, zero->realName, address->realName );
-                cpu_label( _environment, doneLabel );
-
-                value = tmp;
-                
-                break;
-            }
-            case 0:
-                switch( value->type ) {
-                    case VT_FLOAT: {
-                        Variable * address = variable_temporary( _environment, VT_ADDRESS, "(temporary for PRINT)");
-                        Variable * size = variable_temporary( _environment, VT_BYTE, "(temporary for PRINT)");
-                        Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
-
-                        variable_store_string( _environment, tmp->name, "                              " );
-
-                        cpu_dswrite( _environment, tmp->realName );
-
-                        cpu_dsdescriptor( _environment, tmp->realName, address->realName, size->realName );
-
-                        switch( value->precision ) {
-                            case FT_FAST:
-                                cpu_float_fast_to_string( _environment, value->realName, address->realName, size->realName );
-                                break;
-                            case FT_SINGLE:
-                                cpu_float_single_to_string( _environment, value->realName, address->realName, size->realName );
-                                break;
-                        }
-
-                        cpu_dsresize( _environment, tmp->realName, size->realName );
-
-                        value = tmp;
-
-                        break;
-                    }
-                    case VT_IMAGE: {
-                        char bufferName[MAX_TEMPORARY_STORAGE];
-                        sprintf(bufferName, "@image(%s)", value->name);
-                        Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
-                        variable_store_string( _environment, tmp->name, bufferName );
-
-                        value->usedImage = 1;
-                        
-                        value = tmp;
-
-                        break;
-                    }
-                    case VT_IMAGES: {
-                        char bufferName[MAX_TEMPORARY_STORAGE];
-                        sprintf(bufferName, "@images(%s)", value->name);
-                        Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
-                        variable_store_string( _environment, tmp->name, bufferName );
-
-                        value->usedImage = 1;
-
-                        value = tmp;
-
-                        break;
-                    }
-                    case VT_BUFFER: {
-                        char bufferName[MAX_TEMPORARY_STORAGE];
-                        sprintf(bufferName, "@buffer(%s)", value->name);
-                        Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
-                        variable_store_string( _environment, tmp->name, bufferName );
-
-                        value = tmp;
-
-                        break;
-                    }
-                    case VT_SPRITE: {
-                        char bufferName[MAX_TEMPORARY_STORAGE];
-                        sprintf(bufferName, "@sprite(%s)", value->name);
-                        Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
-                        variable_store_string( _environment, tmp->name, bufferName );
-
-                        value = tmp;
-
-                        break;
-                    }
-                    case VT_MSPRITE: {
-                        char bufferName[MAX_TEMPORARY_STORAGE];
-                        sprintf(bufferName, "@msprite(%s)", value->name);
-                        Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
-                        variable_store_string( _environment, tmp->name, bufferName );
-
-                        value = tmp;
-
-                        break;
-                    }
-                    case VT_TILE: {
-                        char bufferName[MAX_TEMPORARY_STORAGE];
-                        sprintf(bufferName, "@tile(%s)", value->name);
-                        Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
-                        variable_store_string( _environment, tmp->name, bufferName );
-
-                        value = tmp;
-
-                        break;
-                    }
-                    case VT_TILESET: {
-                        char bufferName[MAX_TEMPORARY_STORAGE];
-                        sprintf(bufferName, "@tileset(%s)", value->name);
-                        Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
-                        variable_store_string( _environment, tmp->name, bufferName );
-
-                        value->usedImage = 1;
-
-                        value = tmp;
-
-                        break;
-                    }
-                    case VT_TILEMAP: {
-                        char bufferName[MAX_TEMPORARY_STORAGE];
-                        sprintf(bufferName, "@tilemap(%s)", value->name);
-                        Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
-                        variable_store_string( _environment, tmp->name, bufferName );
-
-                        value->usedImage = 1;
-
-                        value = tmp;
-
-                        break;
-                    }
-                    case VT_TILES: {
-                        char bufferName[MAX_TEMPORARY_STORAGE];
-                        sprintf(bufferName, "@tiles(%s)", value->name);
-                        Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
-                        variable_store_string( _environment, tmp->name, bufferName );
-
-                        value = tmp;
-
-                        break;
-                    }
-                    default:
-                        CRITICAL_PRINT_UNSUPPORTED( _value, DATATYPE_AS_STRING[value->type]);
+                    value = tmp;
+                    
+                    break;
                 }
+                case 1: {
+                    Variable * address = variable_temporary( _environment, VT_ADDRESS, "(temporary for PRINT)");
+                    Variable * size = variable_temporary( _environment, VT_BYTE, "(temporary for PRINT)");
+                    Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
+                    Variable * bcheck = variable_temporary( _environment, VT_BYTE, "(temporary for PRINT)");
+                    Variable * zero = variable_temporary( _environment, VT_BYTE, "(temporary for PRINT)");
+                    Variable * one = variable_temporary( _environment, VT_BYTE, "(temporary for PRINT)");
+
+                    cpu_store_8bit( _environment, zero->realName, '0' );
+                    cpu_store_8bit( _environment, one->realName, '1' );
+
+                    variable_store_string( _environment, tmp->name, " " );
+                    cpu_dswrite( _environment, tmp->realName );
+                    cpu_dsdescriptor( _environment, tmp->realName, address->realName, size->realName );
+
+                    MAKE_LABEL
+
+                    char doneLabel[MAX_TEMPORARY_STORAGE]; sprintf( doneLabel, "%sdone", label );
+
+                    outline0(" ; printing bit..." );
+                    cpu_bit_check( _environment, value->realName, value->bitPosition, bcheck->realName, 8 );
+                    cpu_compare_and_branch_8bit_const( _environment, bcheck->realName, 0, label, 1 );
+                    cpu_move_8bit_indirect( _environment, one->realName, address->realName );
+                    cpu_jump( _environment, doneLabel );
+                    cpu_label( _environment, label );
+                    cpu_move_8bit_indirect( _environment, zero->realName, address->realName );
+                    cpu_label( _environment, doneLabel );
+
+                    value = tmp;
+                    
+                    break;
+                }
+                case 0:
+                    switch( value->type ) {
+                        case VT_FLOAT: {
+                            Variable * address = variable_temporary( _environment, VT_ADDRESS, "(temporary for PRINT)");
+                            Variable * size = variable_temporary( _environment, VT_BYTE, "(temporary for PRINT)");
+                            Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
+
+                            variable_store_string( _environment, tmp->name, "                              " );
+
+                            cpu_dswrite( _environment, tmp->realName );
+
+                            cpu_dsdescriptor( _environment, tmp->realName, address->realName, size->realName );
+
+                            switch( value->precision ) {
+                                case FT_FAST:
+                                    cpu_float_fast_to_string( _environment, value->realName, address->realName, size->realName );
+                                    break;
+                                case FT_SINGLE:
+                                    cpu_float_single_to_string( _environment, value->realName, address->realName, size->realName );
+                                    break;
+                            }
+
+                            cpu_dsresize( _environment, tmp->realName, size->realName );
+
+                            value = tmp;
+
+                            break;
+                        }
+                        case VT_IMAGE: {
+                            char bufferName[MAX_TEMPORARY_STORAGE];
+                            sprintf(bufferName, "@image(%s)", value->name);
+                            Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
+                            variable_store_string( _environment, tmp->name, bufferName );
+
+                            value->usedImage = 1;
+                            
+                            value = tmp;
+
+                            break;
+                        }
+                        case VT_IMAGES: {
+                            char bufferName[MAX_TEMPORARY_STORAGE];
+                            sprintf(bufferName, "@images(%s)", value->name);
+                            Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
+                            variable_store_string( _environment, tmp->name, bufferName );
+
+                            value->usedImage = 1;
+
+                            value = tmp;
+
+                            break;
+                        }
+                        case VT_BUFFER: {
+                            char bufferName[MAX_TEMPORARY_STORAGE];
+                            sprintf(bufferName, "@buffer(%s)", value->name);
+                            Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
+                            variable_store_string( _environment, tmp->name, bufferName );
+
+                            value = tmp;
+
+                            break;
+                        }
+                        case VT_SPRITE: {
+                            char bufferName[MAX_TEMPORARY_STORAGE];
+                            sprintf(bufferName, "@sprite(%s)", value->name);
+                            Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
+                            variable_store_string( _environment, tmp->name, bufferName );
+
+                            value = tmp;
+
+                            break;
+                        }
+                        case VT_MSPRITE: {
+                            char bufferName[MAX_TEMPORARY_STORAGE];
+                            sprintf(bufferName, "@msprite(%s)", value->name);
+                            Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
+                            variable_store_string( _environment, tmp->name, bufferName );
+
+                            value = tmp;
+
+                            break;
+                        }
+                        case VT_TILE: {
+                            char bufferName[MAX_TEMPORARY_STORAGE];
+                            sprintf(bufferName, "@tile(%s)", value->name);
+                            Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
+                            variable_store_string( _environment, tmp->name, bufferName );
+
+                            value = tmp;
+
+                            break;
+                        }
+                        case VT_TILESET: {
+                            char bufferName[MAX_TEMPORARY_STORAGE];
+                            sprintf(bufferName, "@tileset(%s)", value->name);
+                            Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
+                            variable_store_string( _environment, tmp->name, bufferName );
+
+                            value->usedImage = 1;
+
+                            value = tmp;
+
+                            break;
+                        }
+                        case VT_TILEMAP: {
+                            char bufferName[MAX_TEMPORARY_STORAGE];
+                            sprintf(bufferName, "@tilemap(%s)", value->name);
+                            Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
+                            variable_store_string( _environment, tmp->name, bufferName );
+
+                            value->usedImage = 1;
+
+                            value = tmp;
+
+                            break;
+                        }
+                        case VT_TILES: {
+                            char bufferName[MAX_TEMPORARY_STORAGE];
+                            sprintf(bufferName, "@tiles(%s)", value->name);
+                            Variable * tmp = variable_temporary( _environment, VT_DSTRING, "(temporary for PRINT)");
+                            variable_store_string( _environment, tmp->name, bufferName );
+
+                            value = tmp;
+
+                            break;
+                        }
+                        default:
+                            CRITICAL_PRINT_UNSUPPORTED( _value, DATATYPE_AS_STRING[value->type]);
+                    }
+            }
+            text_text( _environment, value->name );
+            cpu_dsfree( _environment, value->realName );
+        } else {
+            text_text( _environment, value->name );
         }
-        text_text( _environment, value->name );
-        cpu_dsfree( _environment, value->realName );
-    } else {
-        text_text( _environment, value->name );
+
     }
 
     if ( _new_line ) {
