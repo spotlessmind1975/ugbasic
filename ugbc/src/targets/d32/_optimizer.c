f33f2f5e70ea205f8537b866fd5b7f9b3507edeb
spotlessmind1975 <asimov@mclink.it>
2024-08-24 15:44:46 +0000

    Fixed disruptive optimization rule under 6809.

--- ugbc/src/targets/d32/_optimizer.c
+++ ugbc/src/targets/d32/_optimizer.c
@@ -602,9 +602,7 @@ static void basic_peephole(Environment * _environment, POBuffer buf[LOOK_AHEAD],
     &&  po_buf_match(buf[1], " LDB *", v2)
     &&  po_buf_match(buf[2], " STB *", v3)
     && po_buf_strcmp(v1, v2)==0 ) {
-        optim(buf[0], RULE "(STBa,LDBa,STBb)->(STBb)", NULL );
-        optim(buf[1], NULL, NULL);
-        ++_environment->removedAssemblyLines;
+        optim(buf[1], RULE "(STBa,LDBa,STBb)->(STBb)", NULL );
         ++_environment->removedAssemblyLines;
     }
 
