bc3312789a973da60ea652e94aa35cbcd8f15195
spotlessmind1975 <asimov@mclink.it>
2024-09-02 19:35:48 +0000

    Fixed SOUND to use sid timer under c64.

--- ugbc/src/targets/c64/sound.c
+++ ugbc/src/targets/c64/sound.c
@@ -79,9 +79,11 @@ espressa in millisecondi.
 void sound( Environment * _environment, int _freq, int _delay, int _channels ) {
 
     sid_start( _environment, _channels );
+    sid_set_program( _environment, _channels, IMF_INSTRUMENT_REED_ORGAN );
     sid_set_frequency( _environment, _channels, _freq );
     if ( _delay ) {
-        wait_milliseconds( _environment, _delay );
+        sid_set_duration( _environment, _channels, _delay / 50 /* approx! */ );
+        sid_wait_duration( _environment, _channels );
     }
 
 }
@@ -114,13 +116,24 @@ void sound_vars( Environment * _environment, char * _freq, char * _delay, char *
     if ( _channels ) {
         Variable * channels = variable_retrieve_or_define( _environment, _channels, VT_WORD, 0x07 );
         sid_start_var( _environment, channels->realName );
+        sid_set_program_semi_var( _environment, _channels, IMF_INSTRUMENT_REED_ORGAN );
         sid_set_frequency_vars( _environment, channels->realName, freq->realName );
+        if ( _delay ) {
+            Variable * delay = variable_retrieve_or_define( _environment, _delay, VT_WORD, 0 );
+            Variable * delayScaled = variable_mul2_const( _environment, delay->name, 6 /* approx! */ );
+            sid_set_duration_vars( _environment, channels->realName, delayScaled->realName );
+            sid_wait_duration_vars( _environment, channels->realName );
+        }
     } else {
         sid_start_var( _environment, NULL );
+        sid_set_program_semi_var( _environment, _channels, IMF_INSTRUMENT_REED_ORGAN );
         sid_set_frequency_vars( _environment, NULL, freq->realName );
-    }
-    if ( _delay ) {
-        wait_milliseconds_var( _environment, _delay );
+        if ( _delay ) {
+            Variable * delay = variable_retrieve_or_define( _environment, _delay, VT_WORD, 0 );
+            Variable * delayScaled = variable_mul2_const( _environment, delay->name, 6 /* approx! */ );
+            sid_set_duration_vars( _environment, NULL, delayScaled->realName );
+            sid_wait_duration_vars( _environment, NULL );
+        }
     }
 
 }
