c22c2aee74a455a2c9161a518a4f50564804d2d9
spotlessmind1975 <asimov@mclink.it>
2024-09-03 06:56:24 +0000

    Fixed SOUND to use ted timer under plus4 (untested)p.

--- ugbc/src/targets/plus4/sound.c
+++ ugbc/src/targets/plus4/sound.c
@@ -81,7 +81,8 @@ void sound( Environment * _environment, int _freq, int _delay, int _channels ) {
     ted_start( _environment, _channels );
     ted_set_frequency( _environment, _channels, _freq );
     if ( _delay ) {
-        wait_milliseconds( _environment, _delay );
+        ted_set_duration( _environment, _channels, _delay / 50 /* approx! */);
+        ted_wait_duration( _environment, _channels );
     }
 
 }
@@ -115,12 +116,21 @@ void sound_vars( Environment * _environment, char * _freq, char * _delay, char *
         Variable * channels = variable_retrieve_or_define( _environment, _channels, VT_WORD, 0x07 );
         ted_start_var( _environment, channels->realName );
         ted_set_frequency_vars( _environment, channels->realName, freq->realName );
+        if ( _delay ) {
+            Variable * delay = variable_retrieve_or_define( _environment, _delay, VT_WORD, 0 );
+            Variable * delayScaled = variable_mul2_const( _environment, delay->name, 6 /* approx! */ );
+            ted_set_duration_vars( _environment, channels->realName, delayScaled->realName );
+            ted_wait_duration_vars( _environment, channels->realName );
+        }
     } else {
         ted_start_var( _environment, NULL );
         ted_set_frequency_vars( _environment, NULL, freq->realName );
+        if ( _delay ) {
+            Variable * delay = variable_retrieve_or_define( _environment, _delay, VT_WORD, 0 );
+            Variable * delayScaled = variable_mul2_const( _environment, delay->name, 6 /* approx! */ );
+            ted_set_duration_vars( _environment, NULL, delayScaled->realName );
+            ted_wait_duration_vars( _environment, NULL );
+        }
     }
-    if ( _delay ) {
-        wait_milliseconds_var( _environment, _delay );
-    }
-
+    
 }
