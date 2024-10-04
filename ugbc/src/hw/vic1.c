08efe4d38175d22392e2640629364f2153bab1cc
spotlessmind1975 <asimov@mclink.it>
2024-10-04 17:21:09 +0000

    Added support for IMAGEREF on vic1 (vic20.

--- ugbc/src/hw/vic1.c
+++ ugbc/src/hw/vic1.c
@@ -1645,7 +1645,9 @@ void vic1_put_image( Environment * _environment, Resource * _image, char * _x, c
     deploy( vic1vars, src_hw_vic1_vars_asm);
     deploy( putimage, src_hw_vic1_put_image_asm );
 
-    vic1_load_image_address_to_register( _environment, "TMPPTR", _image, _sequence, _frame, _frame_size, _frame_count );
+    if ( _frame_size ) {
+        vic1_load_image_address_to_register( _environment, "TMPPTR", _image, _sequence, _frame, _frame_size, _frame_count );
+    }
 
     outline1("LDA %s", _x );
     outline0("STA IMAGEX" );
