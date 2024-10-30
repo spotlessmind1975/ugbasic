2686a7966abb270f41f824fc866d74606036cb37
spotlessmind1975 <asimov@mclink.it>
2024-10-30 08:40:01 +0000

    Add support for Sharp PC 1403(H) #951

--- ugbc/src/hw/pc1403/plot.asm
+++ ugbc/src/hw/pc1403/plot.asm
@@ -35,108 +35,56 @@
 ;*                                                                             *
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-; 3000-3039                  3079-3041
-;         ------>                 <------
-; |-----------------------||-----------------------|
-; |7                      ||                       |
-; |                       ...
-; |0                      ||                       |
-; |-----------------------||-----------------------|
-
-; x,y
-; if x > 63 then
-;    px = &h3079 - x
-; else
-;    px = &h3000 + x
-; endif
-; py = 1 << sl(7-y)
-
 ; Calculate the position inside the LCD.
 ; Input:    K = x; L = y
-; Ouput:    Y = address to write to (A=1); A = "OR" mask
+; Ouput:    Y = address to write to; A = "OR" mask
 CALCPOSG:
-
-    ; if x > 63 then
     LP 8
     LDM
     CPIA 63
     JRCP CALCPOSG2
-
-    ;    px = &h3079 - x
 CALCPOSG1:
-    ; save x
     EXAB
-
-    ; A = &H79
     LIA 0x79
     LP 0x03
     EXAM
-
-    ; restore x, B = &H79
     EXAB
-
-    ; B = B(79) - A(x)
     LP 0x04
     SBM
-
-    ; XL = A(79-x)
     LP 0x06
     EXAB
     EXAM
-
-    ; XH = &H30
     LP 0x05
     LIA 0x30
     EXAM
-
     JP CALCPOSDONE
-
-    ; else
-    ;    px = &h3000 + x
 CALCPOSG2:
-    ; XL = A(x)
     LP 0x06
     EXAM
-
-    ; XH = &H30
     LP 0x05
     LIA 0x30
     EXAM
-
     JP CALCPOSDONE
-
-; endif
 CALCPOSDONE:
-    ; J = J - A
-    ; A = 7-y
     LIB 7
     LP 9
     LDM
     LP 3
     SBM 
     EXAM
-
     CPIA 0
     JRZP CALCPOSDONE2
-
-    ; J = A
     LP 1
     EXAM
-
     LIA 1
-
-    ; A = 1 << sl(7-y)
 CALCPOSDONEL1:
     SL
     DECJ
     JRNZM CALCPOSDONEL1
-
     JP CALCPOSDONE3
-
 CALCPOSDONE2:
     LIA 1
 CALCPOSDONE3:
-    ; X = Y
     PUSH
     LIP 6
     LDM
@@ -147,26 +95,22 @@ CALCPOSDONE3:
     LIP 5
     EXAM
     POP 
-
     RTN
 
 ; Plot a point inside the LCD.
-; Input:    I = x; J = y; A = mode
+; Input:    K = x; L = y; A = mode
 PLOT:
-
     CPIA 0
-    JRZP PLOTE                  ;if = 0 then branch to clear the point
+    JRZP PLOTE
     CPIA 1
-    JRZP PLOTD                  ;if = 1 then branch to draw the point
+    JRZP PLOTD
     CPIA 2
-    JRZP PLOTG                  ;if = 2 then branch to get the point (0...3)
+    JRZP PLOTG
     CPIA 3
-    JRZP PLOTC                  ;if = 3 then branch to get the color index (0...15)
+    JRZP PLOTC
     JP PLOTP
-
 PLOTE:
     JP PLOTP
-
 PLOTD:
     CALL CALCPOSG
     EXAB
@@ -178,12 +122,9 @@ PLOTD:
     DY
     IYS     
     JP PLOTP
-
 PLOTG:
     JP PLOTP
-
 PLOTC:
     JP PLOTP
-
 PLOTP:
     RTN
