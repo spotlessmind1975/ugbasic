a0f41e8044bd24fff0cd6bc51f6f423b9553a1cb
spotlessmind1975 <asimov@mclink.it>
2024-11-12 22:54:33 +0000

    Refactorized audio duration under vic20.

--- ugbc/src/targets/vic20/boom.c
+++ ugbc/src/targets/vic20/boom.c
@@ -86,8 +86,8 @@ void boom_var( Environment * _environment, char * _duration, char * _channels ) 
         vic1_set_program_semi_var( _environment, channels->realName, IMF_INSTRUMENT_EXPLOSION );
         if ( _duration ) {
             Variable * duration = variable_retrieve_or_define( _environment, _duration, VT_WORD, 0x07 );
-            cpu_math_div2_const_16bit( _environment, duration->realName, 4, 0 );
-            vic1_set_duration_vars( _environment, channels->realName, duration->realName );
+            Variable * durationInTicks = variable_div_const( _environment, duration->name, 20, NULL );
+            vic1_set_duration_vars( _environment, channels->realName, durationInTicks->realName );
         } else {
             vic1_set_duration_vars( _environment, channels->realName, NULL );
         }
@@ -99,8 +99,8 @@ void boom_var( Environment * _environment, char * _duration, char * _channels ) 
         vic1_set_program_semi_var( _environment, NULL, IMF_INSTRUMENT_EXPLOSION );
         if ( _duration ) {
             Variable * duration = variable_retrieve_or_define( _environment, _duration, VT_WORD, 0x07 );
-            cpu_math_div2_const_16bit( _environment, duration->realName, 4, 0 );
-            vic1_set_duration_vars( _environment, NULL, duration->realName );
+            Variable * durationInTicks = variable_div_const( _environment, duration->name, 20, NULL );
+            vic1_set_duration_vars( _environment, NULL, durationInTicks->realName );
         } else {
             vic1_set_duration_vars( _environment, NULL, NULL );
         }
