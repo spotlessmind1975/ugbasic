42f53e1b8ba9de0f13754133b8ef90b252a31319
spotlessmind1975 <asimov@mclink.it>
2024-11-21 07:12:42 +0000

    Fixed TIMER under plus4.

--- ugbc/src/hw/plus4/startup.asm
+++ ugbc/src/hw/plus4/startup.asm
@@ -92,6 +92,47 @@
 
 ;     RTI
 
+JIFFYUPDATE:
+    PHP
+    PHA
+    TXA
+    PHA
+	LDX	#$00			; clear X
+	INC	$A5		; increment the jiffy clock low byte
+	BNE	JIFFYNORO		; if no rollover ??
+
+	INC	$A4		; increment the jiffy clock mid byte
+	BNE	JIFFYNORO		; branch if no rollover
+
+	INC	$A3		; increment the jiffy clock high byte
+
+					; now subtract a days worth of jiffies from current count
+					; and remember only the Cb result
+JIFFYNORO:
+	SEC				; set carry for subtract
+	LDA	$A5		; get the jiffy clock low byte
+	SBC	#$01			; subtract $4F1A01 low byte
+	LDA	$A4		; get the jiffy clock mid byte
+	SBC	#$1A			; subtract $4F1A01 mid byte
+	LDA	$A3		; get the jiffy clock high byte
+	SBC	#$4F			; subtract $4F1A01 high byte
+	BCC	JIFFYDAY		; if less than $4F1A01 jiffies skip the clock reset
+
+					; else ..
+	STX	$A3		; clear the jiffy clock high byte
+	STX	$A4		; clear the jiffy clock mid byte
+	STX	$A5		; clear the jiffy clock low byte
+					; this is wrong, there are $4F1A00 jiffies in a day so
+					; the reset to zero should occur when the value reaches
+					; $4F1A00 and not $4F1A01. this would give an extra jiffy
+					; every day and a possible TI value of 24:00:00
+JIFFYDAY:
+    PLA
+    TAX
+    PLA
+    PLP
+	RTS
+
 PLUS4STARTUP:
 
     ; Innanzitutto: non conosco il Plus4, quindi potrebbero esserci questioni
