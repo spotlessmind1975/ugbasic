1bf758708742edb09a67ac8e1bf811efa7f8ff32
spotlessmind1975 <asimov@mclink.it>
2024-11-21 21:11:29 +0000

    Addeds support for RUNNING on specific procedures.

--- ugbc/src/targets/common/running.c
+++ ugbc/src/targets/common/running.c
@@ -49,18 +49,22 @@
 @english
 This command lets you know if a thread is in a "running" state or not. A 
 thread is in this state if it has started and has not yet finished its execution.
+Moreover, it is possible to know if a specific procedure is running on a given
+thread, by using the second syntax.
 
 @italian
 Questo comando permette di sapere se un thread si trova in uno stato di "running" o meno. 
 Un thread è in tale stato se è iniziato e ancora non ha terminato la propria esecuzione.
+Inoltre, è possibile sapere se una procedura specifica è in esecuzione su un dato thread, 
+utilizzando la seconda sintassi.
 
-@syntax = RUNNING(thread)
+@syntax = RUNNING(thread[,procedur])
 
 @example IF NOT RUNNING(wheel) THEN
 @example    wheel = SPAWN wheelManager
 @example ENDIF
 </usermanual> */
-Variable * running( Environment * _environment, char * _thread_id ) {
+Variable * running( Environment * _environment, char * _thread_id, char * _procedure ) {
 
     _environment->anyProtothread = 1;
 
@@ -75,7 +79,18 @@ Variable * running( Environment * _environment, char * _thread_id ) {
     Variable * threadId = variable_retrieve_or_define( _environment, _thread_id, VT_THREAD, 0 );
     Variable * state = variable_temporary( _environment, VT_THREAD, "(state)" );
     Variable * result = variable_temporary( _environment, VT_SBYTE, "(is running)" );
+
+    variable_store( _environment, result->name, 0 );
     
+    if ( _procedure ) {
+        Variable * addressRegistered = variable_temporary( _environment, VT_ADDRESS, "(address)");
+        cpu_protothread_get_address( _environment, threadId->realName, addressRegistered->realName );
+        char procedureName[MAX_TEMPORARY_STORAGE]; sprintf( procedureName, "PROC%s", _procedure );
+        Variable * addressProcedure = variable_temporary( _environment, VT_ADDRESS, "(address)");
+        cpu_addressof_16bit( _environment, procedureName, addressProcedure->realName );
+        cpu_compare_and_branch_8bit_const( _environment, variable_compare( _environment, addressRegistered->name, addressProcedure->name )->realName, 0x00, label, 1 );
+    }
+
     cpu_protothread_get_state( _environment, threadId->realName, state->realName );
     variable_compare_and_branch_const( _environment, state->name, PROTOTHREAD_STATUS_RUNNING, protothreadLabel, 1 );
     variable_compare_and_branch_const( _environment, state->name, PROTOTHREAD_STATUS_YIELDED, protothreadLabel, 1 );
