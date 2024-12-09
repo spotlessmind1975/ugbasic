ddca1f9a6b378ab7e1bd298768ff9155457d7f81
spotlessmind1975 <asimov@mclink.it>
2024-12-09 18:44:14 +0000

    Fixed KEYBOARD ASYNC support under pc128op.

--- ugbc/src/targets/pc128op/_var.c
+++ ugbc/src/targets/pc128op/_var.c
@@ -817,7 +817,7 @@ void variable_cleanup( Environment * _environment ) {
     deploy_inplace_preferred( ef936xstartup, src_hw_ef936x_startup_asm);
     deploy_inplace_preferred( putimage, src_hw_ef936x_put_image_asm );
     deploy_inplace_preferred( getimage, src_hw_ef936x_get_image_asm );
-    if ( _environment->keyboardFullSupport ) {
+    if ( _environment->keyboardFullSupport || ! _environment->keyboardConfig.sync ) {
         deploy_inplace_preferred( keyboard, src_hw_pc128op_keyboard_asm );
     } else {
         deploy_inplace_preferred( keyboard, src_hw_pc128op_scancode_asm );
