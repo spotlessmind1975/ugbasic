7432702953ea09bec4b163291aff07436770b7d7
spotlessmind1975 <asimov@mclink.it>
2024-12-10 17:42:58 +0000

    Compiler error with ON ... GOSUB with unexistent label #1084

--- ugbc/src/ugbc.h
+++ ugbc/src/ugbc.h
@@ -3332,6 +3332,8 @@ typedef struct _Environment {
 #define CRITICAL_CANNOT_DUPLICATE_NOT_MPSRITE(n)  CRITICAL2("E334 - cannot init from something that is not MSPRITE", n ); 
 #define CRITICAL_CANNOT_MOVE_PLACEHOLDERS_TO_IMAGE(v)  CRITICAL2("E335 - cannot move PLACEHOLDER to IMAGE", v );
 #define CRITICAL_MID_UNSUPPORTED_FOR_STRING(s)  CRITICAL2("E336 - MID assignment not supported for constant strings", s );
+#define CRITICAL_ON_GOSUB_WITH_NOT_EXISTENT_LABEL(s)  CRITICAL2("E337 - ON...GOSUB with an unexistent label", s );
+#define CRITICAL_ON_GOSUB_WITH_NOT_EXISTENT_LINE_NUMBER(n)  CRITICAL2i("E337 - ON...GOSUB with an unexistent line number", n );
 
 #define CRITICALB( s ) fprintf(stderr, "CRITICAL ERROR during building of %s:\n\t%s\n", ((struct _Environment *)_environment)->sourceFileName, s ); target_cleanup( ((struct _Environment *)_environment) ); exit( EXIT_FAILURE );
 #define CRITICALB2( s, v ) fprintf(stderr, "CRITICAL ERROR during building of %s:\n\t%s (%s)\n", ((struct _Environment *)_environment)->sourceFileName, s, v ); target_cleanup( ((struct _Environment *)_environment) ); exit( EXIT_FAILURE );
