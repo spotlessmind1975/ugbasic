ab055b06d92b4246bdf3428e91f37e6de0bdc024
spotlessmind1975 <asimov@mclink.it>
2024-12-25 22:20:13 +0000

    Preliminary support for new TILEDIMAGE datatype (vic1 and vic20 only).

--- ugbc/src/hw/vic1/put_image.asm
+++ ugbc/src/hw/vic1/put_image.asm
@@ -35,11 +35,692 @@
 ;*                                                                             *
 ;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-; ----------------------------------------------------------------------------
-; - Put image on bitmap
-; ----------------------------------------------------------------------------
+TILESETSLOTFIRST: .byte 0
+TILESETSLOTLAST: .byte 0
+TILESETSLOTUSED: .res 256, 0
+TILESETSLOTATTRIBUTE: .res 256, 0
+
+; ------------------------------------------------------------------------------
+; Allocate a new slot of used tiles.
+;  In : -
+;  Out: -
+; ------------------------------------------------------------------------------
+
+TILESETSLOTALLOC:
+    TYA
+    PHA
+    TXA
+    PHA
+
+    ; First of all, retrieve the offset inside the
+    ; TILESETSLOTUSED for the current last slot.
+
+    LDA TILESETSLOTLAST
+    ASL
+    ASL
+    ASL
+    ASL
+    ASL
+    TAX
+
+    ; Move to the next last slot: if LAST is reached,
+    ; move to the first.
+
+    INC TILESETSLOTLAST
+    LDA TILESETSLOTLAST
+    CMP #8
+    BNE TILESETSLOTALLOCDONE
+    LDA #0
+    STA TILESETSLOTLAST
+TILESETSLOTALLOCDONE:
+
+    ; Next, retrieve the offset inside the
+    ; TILESETSLOTUSED for the actual last slot.
+
+    LDA TILESETSLOTLAST
+    ASL
+    ASL
+    ASL
+    ASL
+    ASL
+    TAY
+
+    ; The TMPPTR2 pointer will contain the base
+    ; address of TILESETSLOTUSED, to be indexed
+    ; by the offset of the actual last slot.
+
+    LDA #<TILESETSLOTUSED
+    STA TMPPTR2
+    LDA #>TILESETSLOTUSED
+    STA TMPPTR2+1
+
+    ; Copy the 32 bytes (256 used tiles) from
+    ; the last to the actual slot.
+
+    LDA #32
+    STA MATHPTR0
+TILESETSLOTALLOCL1:
+    LDA TILESETSLOTUSED,X
+    STA (TMPPTR2),Y
+    INX
+    INY
+    DEC MATHPTR0
+    BNE TILESETSLOTALLOCL1
+
+    PLA
+    TAX
+    PLA
+    TAY
+
+    RTS
+
+; ------------------------------------------------------------------------------
+; Find the first free tile inside the current last slot.
+;  In : -
+;  Out: X - free slot, 255 = no free slot present
+; ------------------------------------------------------------------------------
+
+TILESETSLOTFINDFREE:
+
+    TYA
+    PHA
+
+    LDY #32
+
+    ; Start from the first tile.
+
+    LDA TILESETSLOTLAST
+    ASL
+    ASL
+    ASL
+    ASL
+    ASL
+    TAX
+
+TILESETSLOTFINDFREEL1:
+
+    ; Check if, in the actual group of 8 tiles,
+    ; any tile is free. If such, move to calculate
+    ; the exact number of the tile.
+
+    LDA TILESETSLOTUSED, X
+    CMP #$FF
+    BNE TILESETSLOTFOUNDFREE
+
+    ; Move to the next 8 tiles group.
+
+    INX
+
+    ; Are there more group tiles?
+    ; If so, repeat.
+
+    DEY 
+    BNE TILESETSLOTFINDFREEL1
+
+    ; No free tiles left.
+
+    LDX #255
+
+    PLA
+    TAY
+
+    RTS
+
+TILESETSLOTFOUNDFREE:
+
+    ; Calculate the starting offset of the free tiles,
+    ; that is the ( group index - last ) x 32.
+
+    PHA
+    STX MATHPTR6
+    TXA
+    LSR
+    LSR
+    LSR
+    LSR
+    LSR
+    SEC
+    SBC TILESETSLOTLAST
+    TAX
+    PLA
+
+    LDY #0
+
+TILESETSLOTFOUNDFREEL1:
+
+    ; Check if the right most bit is zero. This means
+    ; that that tile is free.
+
+    LSR
+    BCC TILESETSLOTFOUNDFREEDONE
+
+    ; Increment the offset of the free tile,
+    ; and repeat. This branch should always be
+    ; executed, since the value must 
+    ; have at least one bit to zero.
+
+    INX
+    INY
+    BNE TILESETSLOTFOUNDFREEL1
+
+TILESETSLOTFOUNDFREEDONE:
+
+    TXA
+    PHA
+
+    TYA
+    TAX
+    LDA BITMASK,X
+
+    LDX MATHPTR6
+    ORA TILESETSLOTUSED,X
+    STA TILESETSLOTUSED,X
+    
+    PLA
+    TAX
+
+    PLA
+    TAY
+
+    RTS
+
+; ------------------------------------------------------------------------------
+; REALLOC THE FREE TILES
+;  In : TMPPTR address of TILEDIMAGE
+;  Out: -
+; ------------------------------------------------------------------------------
+
+TILESETSLOTRECALCFREESLOT:
+
+    TYA
+    PHA
+    TXA
+    PHA
+
+    ; We are going to restore the actual slot with the previous one.
+
+    DEC TILESETSLOTLAST
+    LDA TILESETSLOTLAST
+    CMP #$FF
+    BNE TILESETSLOTRECALCFREESLOTA1
+    LDA #7
+    STA TILESETSLOTLAST
+TILESETSLOTRECALCFREESLOTA1:
+    JSR TILESETSLOTALLOC
+
+    ; Now wwe retrieve the offset inside the
+    ; TILESETSLOTUSED for the current first slot.
+
+    LDA TILESETSLOTFIRST
+    ASL
+    ASL
+    ASL
+    ASL
+    ASL
+    TAX
+
+    ; Move to the next first slot: if LAST is reached,
+    ; move to the first.
+
+    INC TILESETSLOTFIRST
+    LDA TILESETSLOTFIRST
+    CMP #8
+    BNE TILESETSLOTRECALCFREEDONE
+    LDA #0
+    STA TILESETSLOTFIRST
+TILESETSLOTRECALCFREEDONE:
+
+    ; Next, retrieve the offset inside the
+    ; TILESETSLOTUSED for the actual last slot.
+
+    LDA TILESETSLOTLAST
+    ASL
+    ASL
+    ASL
+    ASL
+    ASL
+    TAY
+
+    ; The TMPPTR2 pointer will contain the base
+    ; address of TILESETSLOTUSED, to be indexed
+    ; by the offset of the actual last slot.
+
+    LDA #<TILESETSLOTUSED
+    STA TMPPTR2
+    LDA #>TILESETSLOTUSED
+    STA TMPPTR2+1
+
+    ; Mask the 32 bytes (256 used tiles) from
+    ; the first to the actual slot.
+
+    LDA #32
+    STA MATHPTR0
+TILESETSLOTRECALCL2:
+    LDA TILESETSLOTUSED,X
+    EOR #$FF
+    AND (TMPPTR2),Y
+    STA (TMPPTR2),Y
+    INX
+    INY
+    DEC MATHPTR0
+    BNE TILESETSLOTRECALCL2
+
+    PLA
+    TAX
+    PLA
+    TAY
+
+    RTS
+
+; ------------------------------------------------------------------------------
+; PUT IMAGE
+;  In : TMPPTR address of TILEDIMAGE
+;  Out: -
+; ------------------------------------------------------------------------------
+
+PUTIMAGECOUNT:      .BYTE 0
+PUTIMAGEWIDTH:      .BYTE 0
+PUTIMAGEINDEX:      .BYTE 0
 
 PUTIMAGE:
+
+    ; Retrieve the width of the TILEDIMAGE
+
+    LDY #1
+    LDA (TMPPTR), Y
+    STA PUTIMAGEWIDTH
+
+    ; Retrieve if the TILEDIMAGE has been already rendered at least once.
+    LDY #0
+    LDA (TMPPTR),Y
+
+    ; If FLAGS / TIMESLOT == $ff, the TILEDIMAGE has never been rendered,
+    ; so we need to allocate tiles to draw this image.
+
+    CMP #$FF
+    BEQ PUTIMAGEALLOC
+
+    ; Tiles have been already set -- move to draw the image.
+
+    JMP PUTIMAGEDRAW
+
+PUTIMAGEALLOC:
+
+    ; Allocate a new slot of free tiles.
+
+    JSR TILESETSLOTALLOC
+
+PUTIMAGEALLOC2:
+
+    ; Load the size of the image, in terms of tiles.
+
+    LDY #2
+    LDA (TMPPTR),Y
+    STA PUTIMAGECOUNT
+
+    LDY #3
+    STY PUTIMAGEINDEX
+
+PUTIMAGEALLOCL1:
+
+    ; Find the first free tile available.
+
+    JSR TILESETSLOTFINDFREE
+
+    ; If no more tiles are free, we must free the oldest one.
+
+    CPX #255
+    BEQ PUTIMAGEALLOCFL1
+
+    JMP PUTIMAGEALLOCOK
+
+PUTIMAGEALLOCFL1:
+
+    ; Free the oldest tiles.
+
+    JSR TILESETSLOTRECALCFREESLOT
+
+    ; Repeat the allocation procedure
+
+    JMP PUTIMAGEALLOC2
+
+PUTIMAGEALLOCOK:
+
+    ; Set the tile index inside the TILEDIMAGE
+
+    TXA
+    LDY PUTIMAGEINDEX
+    STA (TMPPTR), Y
+
+    ; Now we have to copy the bitmap inside the TILESET.
+    ; This means that we have to calculate the offset 
+    ; inside the TILESET, multiplying the index by 8.
+
+    STA MATHPTR2
+    LDA #0
+    STA MATHPTR3
+
+    ASL MATHPTR2
+    ROL MATHPTR3
+    ASL MATHPTR2
+    ROL MATHPTR3
+    ASL MATHPTR2
+    ROL MATHPTR3
+
+    ; Add the offset to the current TILEDIMAGE starting 
+    ; address, and then we add the current SIZE and,
+    ; finally, the size of the header.
+
+    CLC
+    LDA MATHPTR2
+    ADC TMPPTR
+    STA TMPPTR2
+    LDA MATHPTR3
+    ADC TMPPTR+1
+    STA TMPPTR2+1
+
+    LDY #2
+    CLC
+    LDA TMPPTR2
+    ADC (TMPPTR), Y
+    STA TMPPTR2
+    LDA TMPPTR2+1
+    ADC #0
+    STA TMPPTR2+1
+
+    CLC
+    LDA TMPPTR2
+    ADC #3
+    STA TMPPTR2
+    LDA TMPPTR2+1
+    ADC #0
+    STA TMPPTR2+1
+
+    ; Now we do the same with the hardware TILESET.
+    ; Since we want to optimize the calculation,
+    ; we are going to use a self modify code.
+
+    CLC
+    LDA MATHPTR2
+    ADC PUTIMAGEALLOCOKL1ADDR+1
+    STA PUTIMAGEALLOCOKL1ADDR+1
+    LDA MATHPTR3
+    ADC PUTIMAGEALLOCOKL1ADDR+2
+    STA PUTIMAGEALLOCOKL1ADDR+2
+
+    ; Let's repeat the copy for 8 bytes.
+
+    LDA #8
+    STA MATHPTR0
+    LDX #0
+    LDY #0
+PUTIMAGEALLOCOKL1:
+    LDA (TMPPTR2),Y
+PUTIMAGEALLOCOKL1ADDR:
+    STA $1800, X
+    INX
+    INY
+    DEC MATHPTR0
+    BNE PUTIMAGEALLOCOKL1
+
+    ; Restore the original address of the TILESET.
+
+    LDA #$00
+    STA PUTIMAGEALLOCOKL1ADDR+1
+    LDA #$18
+    STA PUTIMAGEALLOCOKL1ADDR+2
+
+    ; Add the offset to the current TILEDIMAGE starting 
+    ; address, and then we add the current SIZE * 8 and,
+    ; finally, the size of the header.
+
+    CLC
+    LDA MATHPTR2
+    ADC TMPPTR
+    STA TMPPTR2
+    LDA MATHPTR3
+    ADC TMPPTR+1
+    STA TMPPTR2+1
+
+    LDY #2
+    LDA (TMPPTR), Y
+    ASL
+    ASL
+    ASL
+
+    CLC
+    ADC TMPPTR2
+    STA TMPPTR2
+    LDA #0
+    ADC TMPPTR2+1
+    STA TMPPTR2+1
+
+    CLC
+    LDA TMPPTR2
+    ADC #3
+    STA TMPPTR2
+    LDA TMPPTR2+1
+    ADC #0
+    STA TMPPTR2+1
+
+    ; Now we do the same with the attribute table.
+    ; Since we want to optimize the calculation,
+    ; we are going to use a self modify code.
+
+    CLC
+    LDA MATHPTR2
+    ADC PUTIMAGEALLOCOKL2ADDR+1
+    STA PUTIMAGEALLOCOKL2ADDR+1
+    LDA MATHPTR3
+    ADC PUTIMAGEALLOCOKL2ADDR+2
+    STA PUTIMAGEALLOCOKL2ADDR+2
+
+    LDY #0
+    LDX #0
+    LDA (TMPPTR2),Y
+PUTIMAGEALLOCOKL2ADDR:
+    STA TILESETSLOTATTRIBUTE, X
+
+    ; Restore the original address of the ATTRIBUTE.
+
+    LDA #<TILESETSLOTATTRIBUTE
+    STA PUTIMAGEALLOCOKL1ADDR+1
+    LDA #>TILESETSLOTATTRIBUTE
+    STA PUTIMAGEALLOCOKL1ADDR+2
+
+    ; Move to the next index.
+
+    INC PUTIMAGEINDEX
+    DEC PUTIMAGECOUNT
+    BEQ PUTIMAGEALLOCDONE
+    JMP PUTIMAGEALLOCL1
+
+PUTIMAGEALLOCDONE:
+
+    ; Set that the TILEDIMAGE has been already rendered at least once.
+    LDY #0
+    LDA #0
+    STA (TMPPTR),Y
+
+PUTIMAGEDRAW:
+
+    ; Load the size of the image, in terms of tiles.
+
+    LDY #2
+    LDA (TMPPTR),Y
+    STA PUTIMAGECOUNT
+
+    ; Calculate the first coordinates.
+
+    LSR IMAGEX+1
+    ROR IMAGEX
+    LSR IMAGEX+1
+    ROR IMAGEX
+    LSR IMAGEX+1
+    ROR IMAGEX
+
+    LSR IMAGEY+1
+    ROR IMAGEY
+    LSR IMAGEY+1
+    ROR IMAGEY
+    LSR IMAGEY+1
+    ROR IMAGEY
+
+    LDA TEXTADDRESS
+    STA COPYOFTEXTADDRESS
+    LDA TEXTADDRESS+1
+    STA COPYOFTEXTADDRESS+1
+    LDA COLORMAPADDRESS
+    STA COPYOFCOLORMAPADDRESS
+    LDA COLORMAPADDRESS+1
+    STA COPYOFCOLORMAPADDRESS+1
+
+    LDA IMAGEY
+    BEQ PUTIMAGEDRAWL1B
+PUTIMAGEDRAWL1:
+    CLC
+    LDA COPYOFTEXTADDRESS
+    ADC CURRENTTILESWIDTH
+    STA COPYOFTEXTADDRESS
+    LDA COPYOFTEXTADDRESS+1
+    ADC #0
+    STA COPYOFTEXTADDRESS+1
+
+    CLC
+    LDA COPYOFCOLORMAPADDRESS
+    ADC CURRENTTILESWIDTH
+    STA COPYOFCOLORMAPADDRESS
+    LDA COPYOFCOLORMAPADDRESS+1
+    ADC #0
+    STA COPYOFCOLORMAPADDRESS+1
+
+    DEC IMAGEY
+    BNE PUTIMAGEDRAWL1
+
+PUTIMAGEDRAWL1B:
+
+    CLC
+    LDA COPYOFTEXTADDRESS
+    ADC IMAGEX
+    STA COPYOFTEXTADDRESS
+    LDA COPYOFTEXTADDRESS+1
+    ADC #0
+    STA COPYOFTEXTADDRESS+1
+
+    CLC
+    LDA COPYOFCOLORMAPADDRESS
+    ADC IMAGEX
+    STA COPYOFCOLORMAPADDRESS
+    LDA COPYOFCOLORMAPADDRESS+1
+    ADC #0
+    STA COPYOFCOLORMAPADDRESS+1
+
+    ; Now we calculate the starting address inside the TILEDIMAGE
+    ; for the various tiles indexes.
+
+    CLC
+    LDA TMPPTR
+    ADC #3
+    STA TMPPTR2
+    LDA TMPPTR+1
+    ADC #0
+    STA TMPPTR2+1
+
+PUTIMAGEDRAWL2:
+
+    LDY #0
+
+    ; Load the index from the TILEDIMAGE.
+
+    LDA (TMPPTR2), Y
+    TAX
+
+    ; Store the index into the TILEMAP.
+
+    STA (COPYOFTEXTADDRESS), Y
+
+    ; Load the attribute
+
+    LDA TILESETSLOTATTRIBUTE, X
+
+    ; Store the index into the COLORMAP
+
+    STA (COPYOFCOLORMAPADDRESS), Y
+
+    ; Decrement the tiles to copy.
+
+    DEC PUTIMAGECOUNT
+
+    ; If the tiles have been copied, exit!
+
+    BNE PUTIMAGEDRAWL2NEXT
+    JMP PUTIMAGEDRAWDONE
+
+PUTIMAGEDRAWL2NEXT:
+
+    ; Move forward by one tile.
+
+    INC TMPPTR2
+    BNE PUTIMAGEDRAWL2A
+    INC TMPPTR2+1
+PUTIMAGEDRAWL2A:
+
+    INC COPYOFTEXTADDRESS
+    BNE PUTIMAGEDRAWL2B
+    INC COPYOFTEXTADDRESS+1
+PUTIMAGEDRAWL2B:
+
+    INC COPYOFCOLORMAPADDRESS
+    BNE PUTIMAGEDRAWL2C
+    INC COPYOFCOLORMAPADDRESS+1
+PUTIMAGEDRAWL2C:
+
+    ; We reached the width of the TILEDIMAGE?
+    ; No, so we have to repeat.
+
+    CMP PUTIMAGEWIDTH
+    BNE PUTIMAGEDRAWL2
+
+    ; Move forward on the TILEMAP & COLORMAP memory only!
+
+    CLC
+    LDA COPYOFTEXTADDRESS
+    ADC CURRENTTILEMODE
+    STA COPYOFTEXTADDRESS
+    LDA COPYOFTEXTADDRESS+1
+    ADC #0
+    STA COPYOFTEXTADDRESS+1
+
+    SEC
+    LDA COPYOFTEXTADDRESS
+    SBC PUTIMAGEWIDTH
+    STA COPYOFTEXTADDRESS
+    LDA COPYOFTEXTADDRESS+1
+    SBC #0
+    STA COPYOFTEXTADDRESS+1
+
+    CLC
+    LDA COPYOFCOLORMAPADDRESS
+    ADC CURRENTTILEMODE
+    STA COPYOFCOLORMAPADDRESS
+    LDA COPYOFCOLORMAPADDRESS+1
+    ADC #0
+    STA COPYOFCOLORMAPADDRESS+1
+
+    SEC
+    LDA COPYOFCOLORMAPADDRESS
+    SBC PUTIMAGEWIDTH
+    STA COPYOFCOLORMAPADDRESS
+    LDA COPYOFCOLORMAPADDRESS+1
+    SBC #0
+    STA COPYOFCOLORMAPADDRESS+1
+
+    JMP PUTIMAGEDRAWL2
+
+PUTIMAGEDRAWDONE:
+    RTS
+
+PUTIMAGEOLD:
 ;     LDA CURRENTMODE
 ;     ; BITMAP_MODE_STANDARD
 ;     CMP #2
