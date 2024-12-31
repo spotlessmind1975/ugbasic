34c5d4159e7d406d943d4d81ba08ec48cb92ae03
spotlessmind1975 <asimov@mclink.it>
2024-12-31 17:22:27 +0000

    Fixed disruptive optimization rule under 6502.

--- ugbc/src/targets/atari/_optimizer.c
+++ ugbc/src/targets/atari/_optimizer.c
@@ -507,7 +507,8 @@ static void basic_peephole(Environment * _environment, POBuffer buf[LOOK_AHEAD],
 
     if( ! po_buf_match( buf[0], " LDA *,Y", v3 ) && 
         po_buf_match( buf[0], " ST* *", v1, v2 ) && po_buf_match( buf[1], " LD* *", v3, v4 )
-        && strcmp( v1->str, v3->str ) == 0 && strcmp( v2->str, v4->str ) == 0 ) {
+        && strcmp( v1->str, v3->str ) == 0 && strcmp( v2->str, v4->str ) == 0 &&
+        strstr( v2->str, "$FF" ) == NULL ) {
         optim( buf[1], RULE "(STx y, LDx y)->()", NULL );
         ++_environment->removedAssemblyLines;
         ++_environment->removedAssemblyLines;
