69beb8c0caa33d9b49f993f494462cfb4cbe05f2
spotlessmind1975 <asimov@mclink.it>
2025-01-10 13:46:39 +0000

    Fixed need of final DO:RUN PARALLEL:LOOP if ANIMATION/MOVEMENT is used.

--- ugbc/src/targets/common/_cleanups.c
+++ ugbc/src/targets/common/_cleanups.c
@@ -59,7 +59,7 @@ void end_compilation( Environment * _environment ) {
 
     gameloop_cleanup( _environment );
     
-    if ( !_environment->runParallel && _environment->anyProtothread  && !_environment->protothreadForbid ) {
+    if ( _environment->anyProtothread ) {
         begin_do_loop( _environment );
         run_parallel( _environment );
         end_do_loop( _environment );
