1ff7b278416595901025663d40ee238436d1c2c4
spotlessmind1975 <asimov@mclink.it>
2025-01-18 23:32:12 +0000

    IDE crash when trying to use a non-global variable in an ASM block (z80) #1177

--- ugbc/src/targets/coleco/_optimizer.c
+++ ugbc/src/targets/coleco/_optimizer.c
@@ -527,9 +527,18 @@ static void vars_scan(POBuffer buf[LOOK_AHEAD]) {
     if (po_buf_match( buf[0], " LD *, *", arg, tmp) &&
         strstr("A B C D E AD BC DE HL IX IY", strtoupper(arg->str))!=NULL        
         ) { 
-        if(vars_ok(arg)) {
+        if(vars_ok(tmp)) {
+            struct var *v = vars_get(tmp);
+            v->nb_rd++;
+        }
+    }
+
+    if (po_buf_match( buf[0], " LD *,*", arg, tmp) &&
+        strstr("A B C D E AD BC DE HL IX IY", strtoupper(arg->str))!=NULL        
+        ) { 
+        if(vars_ok(tmp)) {
             struct var *v = vars_get(arg);
-            v->nb_wr++;
+            v->nb_rd++;
         }
     }
 
