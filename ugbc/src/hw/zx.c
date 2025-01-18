608189e07fb01095744586b9453898f90166eac2
spotlessmind1975 <asimov@mclink.it>
2025-01-18 18:49:16 +0000

    dded support for WAIT FIRE and emulated joystick on zx.

--- ugbc/src/hw/zx.c
+++ ugbc/src/hw/zx.c
@@ -223,9 +223,43 @@ void zx_wait_key_or_fire_semivar( Environment * _environment, char * _port, int 
 
 void zx_wait_fire( Environment * _environment, int _port, int _release ) {
 
+    if ( _environment->joystickConfig.notEmulated ) {
+
+    } else {
+        
+        _environment->bitmaskNeeded = 1;
+
+        deploy( keyboard, src_hw_zx_keyboard_asm );
+        deploy( joystick, src_hw_zx_joystick_asm );
+
+        if ( _port == -1 ) {
+            outline0("CALL WAITFIRE");
+        } else {
+            outline1("LD A, $%2.2x", (unsigned char)(_port&0xff) );
+            outline0("CALL WAITFIREX");
+        }
+    }
+
 }
 
 void zx_wait_fire_semivar( Environment * _environment, char * _port, int _release ) {
+
+    if ( _environment->joystickConfig.notEmulated ) {
+
+    } else {
+        
+        _environment->bitmaskNeeded = 1;
+
+        deploy( keyboard, src_hw_zx_keyboard_asm );
+        deploy( joystick, src_hw_zx_joystick_asm );
+
+        if ( !_port ) {
+            outline0("CALL WAITFIRE");
+        } else {
+            outline1("LD A, (%s)", _port );
+            outline0("CALL WAITFIREX");
+        }
+    }
 
 }
 
