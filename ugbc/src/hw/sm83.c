53aaf95e7edd082e2fdecd981ba40a10c3c5e2c1
spotlessmind1975 <asimov@mclink.it>
2025-01-24 11:55:15 +0000

    Add support for GAMEBOY (gb) #1120

--- ugbc/src/hw/sm83.c
+++ ugbc/src/hw/sm83.c
@@ -1070,8 +1070,9 @@ void sm83_less_than_8bit_const( Environment * _environment, char *_source, int _
         if ( _signed ) {
 
             outline1("LD A, $%2.2x", _destination);
-            outline0("LD B, A");
+            outline0("LD H, A");
             outline1("LD A, (%s)", _source);
+            outline0("LD D, A");
             if ( _equal ) {
                 outline0("CALL CPULTE8S");
             } else {
