d2cb7caee039f0901b5fbc4656ef6342d1014b60
spotlessmind1975 <asimov@mclink.it>
2025-02-01 13:32:00 +0000

    Fixed reset vars under msx1 when 10 liner is enabled.

--- ugbc/src/targets/msx1/_init.c
+++ ugbc/src/targets/msx1/_init.c
@@ -242,7 +242,7 @@ void target_initialization( Environment * _environment ) {
     
     if ( _environment->tenLinerRulesEnforced ) {
         shell_injection( _environment );
-        outline0("CALL VARINIT2");
+        outline0("CALL VARINIT");
     }
 
 }
