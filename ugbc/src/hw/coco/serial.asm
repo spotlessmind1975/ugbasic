8518b1932db994f2aa3eaf8582065f13053c3eba
spotlessmind1975 <asimov@mclink.it>
2025-02-14 12:18:21 +0000

    Add support for DEFINE SERIAL BECKER / HDBDOS

--- ugbc/src/hw/coco/serial.asm
+++ ugbc/src/hw/coco/serial.asm
@@ -38,13 +38,14 @@
 ; ----------------------------------------------------------------------------
 ; SERIAL READY
 ; ----------------------------------------------------------------------------
-; We call this routine to check if any data is available on DriveWire port.
+; We call this routine to check if any data is available on DriveWire port
+; using becker port emulation under emulator.
 ;
 ; Output:
 ;           Z  = data is available
 ;           NZ = data is unavailable
 ;
-SERIALISREADY
+SERIALBISREADY
     LDA $FF41
     ANDA #$2
     CMPA #$2
@@ -53,33 +54,37 @@ SERIALISREADY
 ; ----------------------------------------------------------------------------
 ; SERIAL READ BYTE
 ; ----------------------------------------------------------------------------
-; We call this routine to read the next byte from serial port. If the byte
-; is not available, the routine will wait for the available data.
+; We call this routine to read the next byte from DriveWire port using becker 
+; port emulation under emulator. If the byte is not available, the routine 
+; will wait for the available data.
 ;
 ; Output:
 ;           A = data received from SERIAL port
 ;
-SERIALREADBYTE
-    JSR SERIALISREADY
-    BNE SERIALREADBYTE
+SERIALBREADBYTE
+    JSR SERIALBISREADY
+    BNE SERIALBREADBYTE
     LDA $FF42
     RTS
 
 ; ----------------------------------------------------------------------------
 ; SERIAL WRITE BYTE
 ; ----------------------------------------------------------------------------
-; We call this routine to write a byte to serial port.
+; We call this routine to write a byte to DriveWire port using becker port 
+; emulation under emulator.
 ;
 ; Input:
 ;           A = data to write to SERIAL server
 ;
-SERIALWRITEBYTE
+SERIALBWRITEBYTE
     STA $FF42
     RTS
 
 ; ----------------------------------------------------------------------------
 ; SERIAL WRITE
 ; ----------------------------------------------------------------------------
+; We call this routine to write a string of bytes to DriveWire port using 
+; becker port emulation under emulator.
 ;
 ; Input:
 ;           X = address of data to send
@@ -88,17 +93,19 @@ SERIALWRITEBYTE
 ;           B = 0 is success, 0xff is failed
 ;
 
-SERIALWRITE
+SERIALBWRITE
     LDA ,X+
-    JSR SERIALWRITEBYTE
+    JSR SERIALBWRITEBYTE
     DECB
-    BNE SERIALWRITE
+    BNE SERIALBWRITE
     LDB #0
     RTS
 
 ; ----------------------------------------------------------------------------
 ; SERIAL WRITE CONST
 ; ----------------------------------------------------------------------------
+; We call this routine to write a sequence of the same byte to DriveWire port 
+; using becker port emulation under emulator.
 ;
 ; Input:
 ;           A = value to write
@@ -107,24 +114,180 @@ SERIALWRITE
 ;           B = 0 is success, 0xff is failed
 ;
 
-SERIALWRITECONST
-    JSR SERIALWRITEBYTE
+SERIALBWRITECONST
+    JSR SERIALBWRITEBYTE
     DECB
-    BNE SERIALWRITECONST
+    BNE SERIALBWRITECONST
     RTS
 
 ; ----------------------------------------------------------------------------
 ; SERIAL READ
 ; ----------------------------------------------------------------------------
+; We call this routine to read a sequence of bytes from DriveWire port 
+; using becker port emulation under emulator.
 ;
 ; Input:
 ;           X = address of data to read to
 ;           B = size of the data to read
 
-SERIALREAD
-    JSR SERIALREADBYTE
+SERIALBREAD
+    JSR SERIALBREADBYTE
     STA ,X+
     DECB
-    BNE SERIALREAD
+    BNE SERIALBREAD
     RTS
 
+; ----------------------------------------------------------------------------
+; ----------------------------------------------------------------------------
+; ----------------------------------------------------------------------------
+
+; ----------------------------------------------------------------------------
+; SERIAL WRITE
+; ----------------------------------------------------------------------------
+; We call this routine to write a string of bytes to DriveWire port using 
+; HDB-DOS, as the regular FujiNet integration.
+;
+; Input:
+;           X = address of data to send
+;           B = size of the data to send
+; Output:
+;           B = 0 is success, 0xff is failed
+;
+
+SERIALHWRITE
+    ORCC #$50
+    LDA RAMENABLED
+    PSHS D
+    CLR RAMENABLED
+    STA $FFDE
+    PSHS X, Y
+    LDA #0
+    TFR D, Y
+    JSR [0xD941]
+    PULS X, Y
+    PULS D
+    STA RAMENABLED
+    BEQ SERIALHWRITEDONE2
+    STA $FFDF
+SERIALHWRITEDONE2
+    TFR CC, B
+    LSRB
+    LSRB
+    ANDB #$01
+    BEQ SERIALHWRITEDONE
+    LDB #$FF
+SERIALHWRITEDONE
+    ANDCC #$AF
+    RTS
+
+; ----------------------------------------------------------------------------
+; SERIAL WRITE CONST
+; ----------------------------------------------------------------------------
+; We call this routine to write a sequence of the same byte to DriveWire port 
+; using HDB-DOS, as the regular FujiNet integration.
+;
+; Input:
+;           A = value to write
+;           B = size of the data to send
+; Output:
+;           B = 0 is success, 0xff is failed
+;
+SERIALHWRITECONSTDATA
+    fcb $00
+SERIALHWRITECONST
+    PSHS D
+    STA SERIALHWRITECONSTDATA
+    LDX #SERIALHWRITECONSTDATA
+    LDB #1
+    JSR SERIALHWRITE
+    PULS D
+    DECB
+    BNE SERIALHWRITECONST
+    RTS
+
+; ----------------------------------------------------------------------------
+; SERIAL READ
+; ----------------------------------------------------------------------------
+; We call this routine to read a sequence of bytes from DriveWire port 
+; using HDB-DOS, as the regular FujiNet integration.
+;
+; Input:
+;           X = address of data to read to
+;           B = size of the data to read
+
+SERIALHREAD
+    ORCC #$50
+    LDA RAMENABLED
+    PSHS D
+    CLR RAMENABLED
+    STA $FFDE
+    PSHS X, Y
+    LDA #0
+    TFR D, Y
+    JSR [0xD93F]
+    PULS X, Y
+
+    PULS D
+    STA RAMENABLED
+    BEQ SERIALHREADDONE2
+    STA $FFDF
+SERIALHREADDONE2
+
+    TFR CC, B
+    LSRB
+    LSRB
+    ANDB #$01
+    BEQ SERIALHREADDONE
+    LDB #$FF
+SERIALHREADDONE
+    ANDCC #$AF
+    RTS
+
+; ----------------------------------------------------------------------------
+; SERIAL READ BYTE
+; ----------------------------------------------------------------------------
+; We call this routine to read a byte from DriveWire port using HBD-DOS, as 
+; the regular FujiNet integration.
+
+SERIALHREADBYTEDATA
+    fcb $00
+SERIALHREADBYTE
+    LDX #SERIALHREADBYTEDATA
+    LDB #1
+    JSR SERIALHREAD
+    LDA SERIALHREADBYTEDATA
+    RTS
+
+SERIALREAD
+    JMP SERIALBREAD
+
+SERIALREADBYTE
+    JMP SERIALBREADBYTE
+
+SERIALWRITE
+    JMP SERIALBWRITE
+
+SERIALWRITECONST
+    JMP SERIALBWRITECONST
+
+SERIALDEFINEBECKERPORT
+    LDD #SERIALBREAD
+    STD (SERIALREAD+1)
+    LDD #SERIALBREADBYTE
+    STD (SERIALREADBYTE+1)
+    LDD #SERIALBWRITE
+    STD (SERIALWRITE+1)
+    LDD #SERIALBWRITECONST
+    STD (SERIALWRITECONST+1)
+    RTS
+
+SERIALDEFINEHDBDOS
+    LDD #SERIALHREAD
+    STD (SERIALREAD+1)
+    LDD #SERIALHREADBYTE
+    STD (SERIALREADBYTE+1)
+    LDD #SERIALHWRITE
+    STD (SERIALWRITE+1)
+    LDD #SERIALHWRITECONST
+    STD (SERIALWRITECONST+1)
+    RTS
