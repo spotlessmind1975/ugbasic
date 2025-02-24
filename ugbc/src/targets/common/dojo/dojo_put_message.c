b27a39b7633c68ebce3aaabc23414e233f24c0b8
spotlessmind1975 <asimov@mclink.it>
2025-02-24 07:31:23 +0000

    Fixed bug on reading data on virtualized fujinet under coco.

--- ugbc/src/targets/common/dojo/dojo_put_message.c
+++ ugbc/src/targets/common/dojo/dojo_put_message.c
@@ -36,6 +36,46 @@
  
  extern char DATATYPE_AS_STRING[][16];
  
+ /* <usermanual>
+@keyword GET MESSAGE
+
+@english
+
+The ''PUT MESSAGE'' statement allows you to send a message to the ''port'' (and optionally 
+the given ''channel''). The message must be a string. If there is an error sending, an 
+error is raised and the statement returns ''FALSE''. Otherwise, it returns ''TRUE''.
+
+@italian
+
+L'istruzione ''PUT MESSAGE'' consente di inviare un messaggio sulla porta (''port'') 
+ed, eventualmente, sul canale (''channel'') dato. Il messaggio deve essere una stringa. 
+In caso di errore nell'invio, viene emesso un errore e l'istruzione restituità ''FALSE''. 
+Altrimenti, restituirà ''TRUE'.
+
+@syntax = [DOJO] PUT MESSAGE( port[, channel], message )
+@syntax [DOJO] PUT MESSAGE port[, channel], message )
+
+@example PUT MESSAGE port, "helllo!"
+
+@alias DOJO PUT MESSAGE
+
+@seeAlso DOJO ERROR
+
+@target atari, coco
+</usermanual> */
+
+/* <usermanual>
+@keyword DOJO PUT MESSAGE
+
+@english
+
+@italian
+
+@alias PUT MESSAGE
+
+@target atari, coco
+</usermanual> */
+
  Variable * dojo_put_message( Environment * _environment, char * _port, char * _channel, char * _message ) {
  
      MAKE_LABEL
