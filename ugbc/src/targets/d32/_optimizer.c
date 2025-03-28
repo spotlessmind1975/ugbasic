d9aa4390a3d5b92544c6398c31d4880e150b39dc
spotlessmind1975 <asimov@mclink.it>
2025-03-28 21:40:47 +0000

    Optimizations under 6809.

--- ugbc/src/targets/d32/_optimizer.c
+++ ugbc/src/targets/d32/_optimizer.c
@@ -854,6 +854,13 @@ static void basic_peephole(Environment * _environment, POBuffer buf[LOOK_AHEAD],
         ++_environment->removedAssemblyLines;
     }
 
+    if( po_buf_match(buf[0], " LDB *", v1)
+    &&  po_buf_match(buf[1], " CLRB" )
+        ) {
+        optim(buf[0], RULE "(BANKREAD1,BANWRITE1)->(BANKREADWRITE1)", NULL );
+        ++_environment->removedAssemblyLines;
+    }
+
 }
 
 /* check if POBuffer matches any of xxyy (used for LDD #$xxyy op) */
