ae9968f6fdc1129848ced002fe5e12db7179f6fb
spotlessmind1975 <asimov@mclink.it>
2025-05-14 19:12:45 +0000

    Moved to version 1.17.2

--- ugbc/src/ugbc.h
+++ ugbc/src/ugbc.h
@@ -60,7 +60,7 @@
  * DECLARATIONS AND DEFINITIONS SECTION 
  ****************************************************************************/
 
-#define UGBASIC_VERSION                 "1.17.1"
+#define UGBASIC_VERSION                 "1.17.2"
 
 #ifdef _WIN32
     #define PATH_SEPARATOR              '\\'
