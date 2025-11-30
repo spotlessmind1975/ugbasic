93324981e3e4d95b661c88f2cbc92c2ef021bc57
spotlessmind1975 <asimov@mclink.it>
2025-11-30 10:01:07 +0000

    Fixed FONT LOAD when using a fixed starting position with no preloaded font.

--- ugbc/src/targets/common/tiles_load.c
+++ ugbc/src/targets/common/tiles_load.c
@@ -234,7 +234,7 @@ Variable * tiles_load( Environment * _environment, char * _filename, int _flags,
         if ( ! _environment->descriptors ) {
             // printf("On demand allocating...\n");
             _environment->descriptors = malloc( sizeof( TileDescriptors ) );
-            _environment->descriptors->count = 0;
+            _environment->descriptors->count = (width/8)*(height/8);
             _environment->descriptors->first = 128;
             _environment->descriptors->firstFree = _environment->descriptors->first;
             _environment->descriptors->lastFree = 255;
