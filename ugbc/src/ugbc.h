7d35567de70d000c38f2b995c6a8b8ea167e90db
spotlessmind1975 <asimov@mclink.it>
2025-12-21 14:28:49 +0000

    Fixed maximum size of descriptors to 512 elements.

--- ugbc/src/ugbc.h
+++ ugbc/src/ugbc.h
@@ -2110,8 +2110,8 @@ typedef struct _TileDescriptors {
     int                 lastFree;
     int                 count;
 
-    TileDescriptor *    descriptor[256];
-    TileData            data[256];
+    TileDescriptor *    descriptor[512];
+    TileData            data[512];
 
 } TileDescriptors;
 
