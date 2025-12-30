b71b5db8d11753c31e602861df9a912d5f1dfdd9
spotlessmind1975 <asimov@mclink.it>
2025-12-30 16:15:38 +0000

    Fixed variable generation under 6502

--- ugbc/src/targets/common/6502/_vars.c
+++ ugbc/src/targets/common/6502/_vars.c
@@ -82,35 +82,35 @@ void vars_emit_constants( Environment * _environment ) {
 
 void vars_emit_byte( Environment * _environment, char * _name, int _value ) {
     if ( _name ) {
-        outline2("%s: .byte $%2.2x", _name, (unsigned char)( _value & 0xff ) );
+        outhead2("%s: .byte $%2.2x", _name, (unsigned char)( _value & 0xff ) );
     } else {
-        outline1(" .byte $%2.2x", (unsigned char)( _value & 0xff ) );
+        outline1(".byte $%2.2x", (unsigned char)( _value & 0xff ) );
     }
 }
 
 void vars_emit_word( Environment * _environment, char * _name, int _value ) {
     if ( _name ) {
-        outline2("%s:  .word $%4.4x", _name, (unsigned int)( _value & 0xffff ) );
+        outhead2("%s:  .word $%4.4x", _name, (unsigned int)( _value & 0xffff ) );
     } else {
-        outline1(" .word $%4.4x", (unsigned int)( _value & 0xffff ) );
+        outline1(".word $%4.4x", (unsigned int)( _value & 0xffff ) );
     }
 }
 
 void vars_emit_dword( Environment * _environment, char * _name, int _value ) {
     if ( _name ) {
-        outline2("%s:  .dword $%8.8x", _name, (unsigned int)( _value & 0xffffffff ) );
+        outhead2("%s:  .dword $%8.8x", _name, (unsigned int)( _value & 0xffffffff ) );
     } else {
-        outline1(" .dword $%8.8x", (unsigned int)( _value & 0xffffffff ) );
+        outline1(".dword $%8.8x", (unsigned int)( _value & 0xffffffff ) );
     }
 }
 
 void vars_emit_number( Environment * _environment, char * _name, int _value ) {
     if ( _name ) {
-        outline2("%s:  .dword $%8.8x", _name, (unsigned int)( _value & 0xffffffff ) );
-        outline1("  .res %d", _environment->numberConfig.maxBytes - 4 );
+        outhead2("%s:  .dword $%8.8x", _name, (unsigned int)( _value & 0xffffffff ) );
+        outline1(".res %d", _environment->numberConfig.maxBytes - 4 );
     } else {
-        outline1(" .dword $%8.8x", (unsigned int)( _value & 0xffffffff ) );
-        outline1("  .res %d", _environment->numberConfig.maxBytes - 4 );
+        outline1(".dword $%8.8x", (unsigned int)( _value & 0xffffffff ) );
+        outline1(".res %d", _environment->numberConfig.maxBytes - 4 );
     }
 }
 
