d86371db3c751e4b92143ce5ceab3e4f0d2f54c4
spotlessmind1975 <asimov@mclink.it>
2025-12-30 20:41:59 +0000

    Optimized code generation for 6809.

--- ugbc/src/targets/d32/_optimizer.c
+++ ugbc/src/targets/d32/_optimizer.c
@@ -917,6 +917,13 @@ static void basic_peephole(Environment * _environment, POBuffer buf[LOOK_AHEAD],
         ++_environment->removedAssemblyLines;
     }
 
+    if( po_buf_match(buf[0], " LDB #$*", v1)
+    &&  po_buf_match(buf[1], " LDD #$*", v2)
+        ) {
+        optim(buf[0], RULE "(LDB#,LDD#)->(LDD)", NULL );
+        ++_environment->removedAssemblyLines;
+    }
+
     if( po_buf_match(buf[0], " B* *", v1, v2)
     &&  ( po_buf_match(buf[1], " DECB" ) || po_buf_match(buf[1], " INCB" ) )
     &&  po_buf_match(buf[3], " *", v5)
