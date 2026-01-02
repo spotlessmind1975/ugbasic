94fd7551ac52ce859ad3da565c69288fffcc8f4d
spotlessmind1975 <asimov@mclink.it>
2026-01-02 16:16:31 +0000

    Optimized peephole for 6809.

--- ugbc/src/targets/d32/_optimizer.c
+++ ugbc/src/targets/d32/_optimizer.c
@@ -935,6 +935,11 @@ static void basic_peephole(Environment * _environment, POBuffer buf[LOOK_AHEAD],
         ++_environment->removedAssemblyLines;
     }
 
+    if( po_buf_match(buf[0], " LDD #0000")
+        ) {
+        optim(buf[0], RULE "(LDD#0000)->(CLRA, CLRB)", "\tCLRA\n\tCLRB" );
+    }
+
     if( po_buf_match(buf[0], " LDD #$*", v1)
     &&  po_buf_match(buf[1], " LDB #$*", v3, v2)
         ) {
