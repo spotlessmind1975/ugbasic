7892bcdea14eb5723ed02f830ebbcbfe2fbcea1d
spotlessmind1975 <asimov@mclink.it>
2026-02-08 15:16:58 +0000

    Corrected bank access under mo5.

--- ugbc/src/targets/mo5/bank_read.c
+++ ugbc/src/targets/mo5/bank_read.c
@@ -112,6 +112,7 @@ void bank_read_vars( Environment * _environment, char * _bank, char * _address1,
     outline1("LDX %s", address2->realName );
     outline1("LDU %s", size->realName );
     outline0("JSR BANKREAD");
+    _environment->bankAccessOptimization.readn = 1;
     outline0("; end bank read");
 
 }
@@ -132,6 +133,7 @@ void bank_read_vars_direct( Environment * _environment, char * _bank, char * _ad
     outline1("LDX #%s", _address2 );
     outline1("LDU %s", size->realName );
     outline0("JSR BANKREAD");
+    _environment->bankAccessOptimization.readn = 1;
     outline0("; end bank read");
 
 }
