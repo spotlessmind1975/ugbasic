71f424939ee92e3b3af73fa8884b4f5d71e71aa0
spotlessmind1975 <asimov@mclink.it>
2026-05-02 21:51:30 +0000

    Made async keyboard default for to8.

--- ugbc/src/hw/to8.h
+++ ugbc/src/hw/to8.h
@@ -130,7 +130,7 @@
 #define KEY_RIGHT                    0x09
 #define KEY_DIVISION                 '/'
 
-#define KEYBOARD_CONFIG_DEFAULT_SYNC    1
+#define KEYBOARD_CONFIG_DEFAULT_SYNC    0
 
 #define         JOY_COUNT           0
 #define JOYSTICK_0					0
