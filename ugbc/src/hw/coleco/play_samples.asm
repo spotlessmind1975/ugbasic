826175b5abd14f25087998e44ba56bf99b49e9f1
spotlessmind1975 <asimov@mclink.it>
2026-08-30 19:34:29 +0000

    Added support for PLAY SAMPLES under coleco.

---
+++ ugbc/src/hw/coleco/play_samples.asm
@@ -0,0 +1,63 @@
+; /*****************************************************************************
+;  * ugBASIC - an isomorphic BASIC language compiler for retrocomputers        *
+;  *****************************************************************************
+;  * Copyright 2021-2026 Marco Spedaletti (asimov@mclink.it)
+;  *
+;  * Licensed under the Apache License, Version 2.0 (the "License
+;  * you may not use this file eXcept in compliance with the License.
+;  * You may obtain a copy of the License at
+;  *
+;  * http://www.apache.org/licenses/LICENSE-2.0
+;  *
+;  * Unless required by applicable law or agreed to in writing, software
+;  * distributed under the License is distributed on an "AS IS" BASIS,
+;  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either eXpress or implied.
+;  * See the License for the specific language governing permissions and
+;  * limitations under the License.
+;  *----------------------------------------------------------------------------
+;  * Concesso in licenza secondo i termini della Licenza Apache, versione 2.0
+;  * (la "Licenza è proibito usare questo file se non in conformità alla
+;  * Licenza. Una copia della Licenza è disponibile all'indirizzo:
+;  *
+;  * http://www.apache.org/licenses/LICENSE-2.0
+;  *
+;  * Se non richiesto dalla legislazione vigente o concordato per iscritto,
+;  * il software distribuito nei termini della Licenza è distribuito
+;  * "COSì COM'è", SENZA GARANZIE O CONDIZIONI DI ALCUN TIPO, esplicite o
+;  * implicite. Consultare la Licenza per il testo specifico che regola le
+;  * autorizzazioni e le limitazioni previste dalla medesima.
+;  ****************************************************************************/
+;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;*                                                                             *
+;*                        PLAY SAMPLES ON COLECOVISION                         *
+;*                                                                             *
+;*                             by Marco Spedaletti                             *
+;*                                                                             *
+;* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+PLAYSAMPLES:
+	LD A, %10000001
+	OUT ($FF), A
+	LD A, %00000000
+	OUT ($FF), A
+
+PLAYSAMPLESL1:
+	LD A, (HL)
+
+	CP 0
+	JR Z, PLAYSAMPLESDONE
+    
+    OR %10010000            
+    
+    OUT ($FF), A
+	INC HL
+	LD B, 30
+PLAYSAMPLESL2:	
+	DEC B
+	JR NZ, PLAYSAMPLESL2
+	JR PLAYSAMPLESL1
+
+PLAYSAMPLESDONE:
+	LD A, %11111111
+	OUT ($FF), A
+	RET
